46f9eee8c3959884b3ec3958d80cf50dbdb76a6b
Tarik El-Yassem <tarik@openvas.org>
2006-06-01 15:09:19 +0000

    Icluded header.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@142 423fd1db-d629-0410-8442-d21db03e70f4

--- nmake.bat
+++ nmake.bat
@@ -1,2 +1,27 @@
+::  OpenVAS
+::  $Id$
+::  Description: runs nmake.
+::
+::  Authors: - Jordan Hrycaj <jordan@mjh.teddy-net.com> (Original develoment)
+::           - Tarik El-Yassem <mailto:tarik@openvas.org> (Headers section)
+::
+::  Copyright:
+::  Portions Copyright (C) 2006 Software in the Public Interest, Inc.
+::  Based on work Copyright (C) 1996 - 1999 mjh-EDV Beratung
+::
+::  This program is free software; you can redistribute it and/or modify
+::  it under the terms of the GNU General Public License version 2,
+::  as published by the Free Software Foundation
+::
+::  This program is distributed in the hope that it will be useful,
+::  but WITHOUT ANY WARRANTY; without even the implied warranty of
+::  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::  GNU General Public License for more details.
+::
+::  You should have received a copy of the GNU General Public License
+::  along with this program; if not, write to the Free Software
+::  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 nmake.exe /nologo /f nmake.w32 %1 %2 %3 %4 %5 %6 %7 %8 %9
 
