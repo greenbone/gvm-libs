b862ad4e9f4a774490a3750adbbbdf7dcef88764
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2008-03-14 08:54:40 +0000

    * include/libopenvas.h: Annotated module names, some lines reordered, removed W32 conditional.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@716 423fd1db-d629-0410-8442-d21db03e70f4

--- include/libopenvas.h
+++ include/libopenvas.h
@@ -24,8 +24,6 @@
 #define ExtFunc
 #endif
 
-ExtFunc void nessus_lib_version(int *, int *, int *);
- 
 /*
  * Arglist definition and defines
  */
@@ -63,41 +61,51 @@ typedef int(*plugin_run_t)(struct arglist *);
  */
 
 /* Plugin specific network functions */
+/* network.c */
 ExtFunc int open_sock_tcp(struct arglist * , unsigned int, int );
 ExtFunc int open_sock_udp(struct arglist * , unsigned int );
 ExtFunc int open_sock_option(struct arglist * , unsigned int , int , int, int);
 ExtFunc int recv_line(int, char *, size_t);
+
+/* network.c */
 ExtFunc int nrecv(int, void*, int, int);
 ExtFunc int socket_close(int);
 
 /* Additional functions -- should not be used by the plugins */
+/* network.c */
 ExtFunc int open_sock_tcp_hn(const char * , unsigned int );
 ExtFunc int open_sock_opt_hn(const char * , unsigned int , int , int, int );
+
 ExtFunc struct in_addr nn_resolve (const char *); 
 
+/* network.c */
 #ifdef __GNUC__
 ExtFunc void auth_printf(struct arglist *, char * , ...) __attribute__ (( format (printf, 2, 3)));
 #else
 ExtFunc void auth_printf(struct arglist *, char * , ...);
 #endif
+
+/* plugutils.c */
 ExtFunc void scanner_add_port(struct arglist*, int, char *);
+
+/* network.c */
 ExtFunc void auth_send(struct arglist *, char *);
 ExtFunc char * auth_gets(struct arglist *, char * , size_t);
-ExtFunc int ping_host(struct in_addr);
 
-ExtFunc unsigned short *getpts(char *, int *);
+ExtFunc int ping_host(struct in_addr);
 
 /* 
  * Management of the arglists --should not be used directly by
  * the plugins
  */
 
+/* arglist.c */
 ExtFunc void arg_add_value(struct arglist *, const char *, int, long, void *);	   
 ExtFunc int arg_set_value(struct arglist *, const char *, long, void *);	
 ExtFunc int arg_set_type(struct arglist *, const char *, int);
 ExtFunc void * arg_get_value(struct arglist *, const char *);
-ExtFunc int arg_get_type(struct arglist *, const char *);
 ExtFunc int arg_get_length(struct arglist *, const char *);
+ExtFunc int arg_get_type(struct arglist *, const char *);
 ExtFunc void arg_dump(struct arglist *, int);
 ExtFunc void arg_dup(struct arglist *, struct arglist *);
 ExtFunc void arg_free(struct arglist *);
@@ -110,62 +118,79 @@ ExtFunc void arg_free_name(char*);
  * Arglist management at plugin-level
  */
  
+/* plugutils.c */
 ExtFunc void plug_set_name(struct arglist *, const char *, const char *);
 ExtFunc char*plug_get_name(struct arglist*);
 
+/* plugutils.c */
 ExtFunc void plug_set_path(struct arglist *, const char *);
 ExtFunc char*plug_get_path(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_fname(struct arglist *, const char *);
 ExtFunc char*plug_get_fname(struct arglist *);
 
-
+/* plugutils.c */
 ExtFunc void plug_set_version(struct arglist *, const char *);
 ExtFunc char*plug_get_version(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_timeout(struct arglist *, int);
 ExtFunc int  plug_get_timeout(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_launch(struct arglist *, int);
 ExtFunc int plug_get_launch(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_summary(struct arglist *, const char *, const char*);
 ExtFunc char*plug_get_summary(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_description(struct arglist *, const char *,const char *);
 ExtFunc char*plug_get_description(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_category(struct arglist *, int);
 ExtFunc int  plug_get_category(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_copyright(struct arglist *, const char *, const char*);
 ExtFunc char*plug_get_copyright(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_family(struct arglist * , const char *, const char *);
 ExtFunc char*plug_get_family(struct arglist *);
 
+/* plugutils.c */
 ExtFunc	void plug_set_dep(struct arglist *, const char *);
 ExtFunc struct arglist * plug_get_deps(struct arglist*);
 
+/* plugutils.c */
 ExtFunc void plug_set_id(struct arglist *, int);
 ExtFunc int  plug_get_id(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_cve_id(struct arglist *, char *);
 ExtFunc char*plug_get_cve_id(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_bugtraq_id(struct arglist *, char *);
 ExtFunc char*plug_get_bugtraq_id(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_xref(struct arglist *, char *, char *);
 ExtFunc char * plug_get_xref(struct arglist *);
 
 ExtFunc void plug_set_see_also(struct arglist *, char *);
 ExtFunc struct arglist * plug_get_see_also(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_set_ssl_cert(struct arglist*, char*);
 ExtFunc void plug_set_ssl_key(struct arglist*, char*);
 ExtFunc void plug_set_ssl_pem_password(struct arglist*, char*);
 
+/* network.c */
 ExtFunc int nessus_SSL_init(char*);
 
 
@@ -173,28 +198,37 @@ ExtFunc void plug_add_dep(struct arglist *, char *, char *);
 
 ExtFunc void plug_add_port(struct arglist *, int);
 
+/* plugutils.c */
 ExtFunc const char * plug_get_hostname(struct arglist *);
 ExtFunc const char * plug_get_host_fqdn(struct arglist *);
-ExtFunc unsigned int plug_get_host_open_port(struct arglist * desc);
 ExtFunc void plug_add_host(struct arglist *, struct arglist *);
+ExtFunc unsigned int plug_get_host_open_port(struct arglist * desc);
 
+/* plugutils.c */
 ExtFunc char* plug_get_cve_id(struct arglist*);
 ExtFunc char* plug_get_bugtraq_id(struct arglist*);
 
+/* plugutils.c */
 ExtFunc void plug_require_key(struct arglist *, const char *);
 ExtFunc struct arglist * plug_get_required_keys(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_exclude_key(struct arglist *, const char *);
 ExtFunc struct arglist * plug_get_excluded_keys(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_require_port(struct arglist *, const char *);
 ExtFunc struct arglist * plug_get_required_ports(struct arglist *);
 
+/* plugutils.c */
 ExtFunc void plug_require_udp_port(struct arglist*, const char *);
 ExtFunc struct arglist * plug_get_required_udp_ports(struct arglist *);
-
 ExtFunc int plug_get_port_transport(struct arglist*, int);
+
+/* scanners_utils.c */
 ExtFunc int comm_send_status(struct arglist*, char*, char*, int, int);
+ExtFunc unsigned short *getpts(char *, int *);
+
 ExtFunc int islocalhost(struct in_addr *);
 
 
@@ -204,38 +238,41 @@ ExtFunc int islocalhost(struct in_addr *);
  */
  
 /* Plugin-specific : */
+/* plugutils.c */
 ExtFunc void proto_post_hole(struct arglist *, int, const char *, const char *);
 ExtFunc void post_hole(struct arglist *, int, const char *);
 ExtFunc void post_hole_udp(struct arglist *, int, const char *);
 #define post_hole_tcp post_hole
 
+/* plugutils.c */
 ExtFunc void proto_post_info(struct arglist *, int, const char *, const char *);
 ExtFunc void post_info(struct arglist *, int, const char *);
 ExtFunc void post_info_udp(struct arglist *, int, const char *);
 #define post_info_tcp post_info
 
+/* plugutils.c */
 ExtFunc void proto_post_note(struct arglist *, int, const char *, const char *);
 ExtFunc void post_note(struct arglist *, int, const char *);
 ExtFunc void post_note_udp(struct arglist *, int, const char *);
 #define post_note_tcp post_note
 
-#ifndef _WIN32
 /* returns a full duplex data file stream */
 ExtFunc FILE * ptyexecvp (const char *file, const char **argv, pid_t *child);
-#endif /* _WIN32 */
-ExtFunc char ** append_argv (char **argv, char *opt);
-ExtFunc void    destroy_argv (char **argv);
+
 ExtFunc void (*pty_logger(void(*)(const char *, ...)))(const char *, ...);
 
+/* popen.c */
 ExtFunc FILE*	nessus_popen(const char*, char *const[], pid_t*);
 ExtFunc FILE*	nessus_popen4(const char*, char *const[], pid_t*, int);
 ExtFunc int	nessus_pclose(FILE*, pid_t);
+ExtFunc char ** append_argv (char **argv, char *opt);
+ExtFunc void    destroy_argv (char **argv);
 
 /* 
  * Management of the portlists
  */
 
-
+/* plugutils.c */
 ExtFunc void host_add_port(struct arglist *, int, int);
 ExtFunc void host_add_port_udp(struct arglist *, int, int);
 ExtFunc int host_get_port_state(struct arglist *, int);
@@ -253,17 +290,19 @@ char * host_get_port_banner(struct arglist *, int);
  * Miscellaneous functions
  */
  
+/* plugutils.c */
 ExtFunc struct in_addr * plug_get_host_ip(struct arglist *);
-ExtFunc char * plug_get_host_name(struct arglist *);
 ExtFunc char * get_preference(struct arglist *, const char *);
+ExtFunc void add_plugin_preference(struct arglist *, const char *, const char *, const char *);
+ExtFunc char *get_plugin_preference(struct arglist *, const char *);
+ExtFunc const char *get_plugin_preference_fname(struct arglist*, const char*);
+
+ExtFunc char * plug_get_host_name(struct arglist *);
 #define PREF_CHECKBOX "checkbox"
 #define PREF_ENTRY "entry"
 #define PREF_RADIO "radio"
 #define PREF_PASSWORD "password"
 #define PREF_FILE "file"
-ExtFunc void add_plugin_preference(struct arglist *, const char *, const char *, const char *);
-ExtFunc char *get_plugin_preference(struct arglist *, const char *);
-ExtFunc const char *get_plugin_preference_fname(struct arglist*, const char*);
 
 /*
  * Replacement for system related functions
@@ -302,24 +341,27 @@ ExtFunc void * __hml_realloc(char*, int, void*, size_t);
 /* 
  * Inter Plugins Communication functions
  */
-ExtFunc struct kb_item ** plug_get_kb(struct arglist *);
+
+/* plugutils.c */
 ExtFunc void plug_set_key(struct arglist *, char *, int, void *);
 ExtFunc void plug_replace_key(struct arglist *, char *, int, void *);
-ExtFunc void * plug_get_key(struct arglist *, char *, int *);
 ExtFunc void * plug_get_fresh_key(struct arglist *, char *, int *);
+ExtFunc struct kb_item ** plug_get_kb(struct arglist *);
+ExtFunc void * plug_get_key(struct arglist *, char *, int *);
+ExtFunc void * plug_get_key(struct arglist *, char *, int *);
+
 /*
  * FTP Functions
  */
+/* ftp_funcs.c */
 ExtFunc int ftp_log_in(int , char * , char * );
 ExtFunc int ftp_get_pasv_address(int , struct sockaddr_in * );
 
-
+/* plugutils.c */
+ExtFunc char* nessuslib_version();
+ExtFunc void nessus_lib_version(int *, int *, int *);
 ExtFunc char* addslashes(char*);
 ExtFunc char* rmslashes(char*);
-
-ExtFunc char* nessuslib_version();
-
-
 
 /*
  * Pcap utils
@@ -384,30 +426,38 @@ ExtFunc int get_mac_addr(struct in_addr, char**);
 #define NESSUS_CNX_IDS_EVASION_SEND_MASK (NESSUS_CNX_IDS_EVASION_SPLIT|NESSUS_CNX_IDS_EVASION_INJECT|NESSUS_CNX_IDS_EVASION_SHORT_TTL)
 
 
+/* network.c */
 ExtFunc int    open_stream_connection(struct arglist *, unsigned int, int, int);
 ExtFunc int    open_stream_connection_unknown_encaps(struct arglist *, unsigned int, int, int *);
 ExtFunc int    open_stream_connection_unknown_encaps5(struct arglist *, unsigned int, int, int *, int *);
 ExtFunc int    open_stream_auto_encaps(struct arglist *, unsigned int, int);
+
+
+/* network.c */
 ExtFunc int    write_stream_connection (int, void * buf, int n);
 ExtFunc int    read_stream_connection (int, void *, int);
 ExtFunc int    read_stream_connection_min(int, void*, int, int);
-ExtFunc int    close_stream_connection(int);
 ExtFunc int    nsend(int, void*, int, int);
+ExtFunc int    close_stream_connection(int);
 ExtFunc const char* get_encaps_name(int);
 ExtFunc const char* get_encaps_through(int);
 
+/* network.c */
 ExtFunc int    stream_set_timeout(int, int);
 ExtFunc int    stream_set_options(int, int, int);
 
+/* network.c */
 ExtFunc int	stream_set_buffer(int, int);
 ExtFunc int	stream_get_buffer_sz (int);
 ExtFunc int	stream_get_err(int);
 
+/* network.c */
 ExtFunc	       void*   stream_get_ssl(int);
 
 struct ovas_server_context_s;
 typedef struct ovas_server_context_s * ovas_server_context_t;
 
+/* network.c */
 ovas_server_context_t ovas_server_context_new(int encaps,
 					      const char* certfile,
 					      const char* keyfile,
@@ -415,22 +465,27 @@ ovas_server_context_t ovas_server_context_new(int encaps,
 					      const char* cacertfile,
 					      int force_pubkey_auth);
 void ovas_server_context_free(ovas_server_context_t);
-int ovas_open_server_socket(ovas_server_context_t);
 int ovas_server_context_attach(ovas_server_context_t ctx, int soc);
 
+int ovas_open_server_socket(ovas_server_context_t);
 
-ExtFunc	int 	is_shell_command_present(char*);
+/* plugutils.c */
 ExtFunc char* 	find_in_path(char*, int);
+ExtFunc	int 	is_shell_command_present(char*);
 
+/* www_funcs: */
 ExtFunc char*	build_encode_URL(struct arglist*, char*, char*, char*, char*);
 
 
+/* network.c */
 ExtFunc int nessus_register_connection(int, void*);
 ExtFunc int nessus_deregister_connection(int);
 ExtFunc int nessus_get_socket_from_connection(int);
 
 
 ExtFunc void nessus_init_random();
+
+/* network.c */
 ExtFunc int stream_zero(fd_set*);
 ExtFunc int stream_set(int, fd_set*);
 ExtFunc int stream_isset(int, fd_set*);
@@ -443,24 +498,28 @@ ExtFunc u_char* bpf_next_tv(int, int *, struct timeval *);
 ExtFunc void bpf_close(int);
 ExtFunc int  bpf_datalink(int);
 
+/* proctitle.c */
 void initsetproctitle(int argc, char *argv[], char *envp[]);
 #ifndef HAVE_SETPROCTITLE
 void setproctitle( const char *fmt, ... );
 #endif
 
 
-
+/* network.c */
 ExtFunc struct in_addr socket_get_next_source_addr();
 ExtFunc int set_socket_source_addr(int, int);
 ExtFunc void socket_source_init(struct in_addr *);
 
+/* store.c */
 struct arglist * store_plugin(struct arglist *,  char *);
 struct arglist * store_load_plugin(char *, char *,  struct arglist*);
 int		 store_init_sys(char *);
 int		 store_init_user(char *);
 
 
+/* services1.c */
 int		nessus_init_svc();
+
 /*-----------------------------------------------------------------*/
 
 #define KB_TYPE_INT ARG_INT
@@ -476,26 +535,23 @@ struct kb_item {
 	struct kb_item * next;
 };
 
-
+/* kb.c */
 struct kb_item ** kb_new();
-
-
 struct kb_item * kb_item_get_single(struct kb_item **, char *, int );
 char * kb_item_get_str(struct kb_item **, char *);
 int    kb_item_get_int(struct kb_item **, char *);
-
 struct kb_item * kb_item_get_all(struct kb_item **, char *);
 struct kb_item * kb_item_get_pattern(struct kb_item **, char *);
 void   kb_item_get_all_free(struct kb_item *);
 
-
+/* kb.c */
 int    kb_item_add_str(struct kb_item **, char *, char *);
 int    kb_item_set_str(struct kb_item **, char *, char *);
 int    kb_item_add_int(struct kb_item **, char *, int   );
 int    kb_item_set_int(struct kb_item **, char *, int   );
-
 void   kb_item_rm_all(struct kb_item **, char *);
 
+/* kb.c */
 struct arglist * plug_get_oldstyle_kb(struct arglist * );
 
 
@@ -524,7 +580,7 @@ struct http_msg * http_share_send_recv_msg(struct arglist *, struct http_msg *);
 pid_t http_share_init(struct arglist *);
 int http_share_close(struct arglist *, pid_t);
 
-
+/* network.c */
 int os_send(int, void*, int, int);
 int os_recv(int, void*, int, int);
 
@@ -556,21 +612,24 @@ int os_recv(int, void*, int, int);
 #define INTERNAL_COMM_SHARED_SOCKET_ERROR	64
 
 
-
+/* network.c */
 int internal_send(int, char *, int);
 int internal_recv(int, char **, int *, int *);
+
 int internal_finished(int);
-	
+
+/* network.c */
 int fd_is_stream(int);
 int stream_pending(int);
 
+/* share_fd.c */
 int send_fd(int, int);
 int recv_fd(int);
 
+/* plugutils.c */
 int shared_socket_register ( struct arglist *, int, char *);
 int shared_socket_acquire  ( struct arglist *, char * );
 int shared_socket_release  ( struct arglist *, char * );
 int shared_socket_destroy  ( struct arglist *, char * );
 
 #endif
-
