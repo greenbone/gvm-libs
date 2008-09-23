546797d4b7a67b0b5d187d959a85b1ecd8ec677c
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2008-09-23 19:20:15 +0000

    * libopenvas/ntp.h: Removed elements "md5_caching", "ntp_11", "plugins_oid", and "plugins_version" from struct ntp_caps.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@1411 423fd1db-d629-0410-8442-d21db03e70f4

--- libopenvas/ntp.h
+++ libopenvas/ntp.h
@@ -28,27 +28,12 @@
 
 #define OTP_10 100  /* OTP/1.0 */
 
-/* Those marked a "deprecated" can be removed, once openvas-libraries
- * is branched for 1.1  */
 typedef struct {
   int ntp_version; /*  NTP_VERSION, as defined in ntp.h */
-  int ntp_11:1;    /*  TRUE, if we may use NTP 1.1 features; should
-                       better be splitted into different capability
-                       attributes, but this one simplifies the step
-                       from NTP 1.1 to NTP 1.2. In the future we'll
-                       use caps, I promise! :-) */
   int scan_ids:1;  /*  TRUE, if HOLE and INFO messages should
                        contain scan ID's. */
   int pubkey_auth:1; /* TRUE if the client wants to use public key
                         authentification */
-
-  int md5_caching:1; /* TRUE if the client does not want us to send the
-                        list of plugins directly, but just the md5 
-                        hash instead */
-
-  int plugins_version:1; /* TRUE if the client wants us to send the versions
-                            of our plugins */
-  int plugins_oid:1; /* the OID of the plugins along with their version */
   int dns:1; /* send the host name and host ip */
 } ntp_caps;
 
