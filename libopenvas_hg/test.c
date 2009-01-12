645c8e059db43c2654f8db929951d634b8334a86
Felix Wolfsteller <felix.wolfsteller@greenbone.net>
2009-01-12 12:59:40 +0000

    Added GPL headers.
    * libopenvas_hg/hg_debug.c, libopenvas_hg/hg_filter.h,
    libopenvas_hg/test.c, libopenvas_hg/hg_utils.h,
    libopenvas_hg/hg_subnet.h, libopenvas_hg/hg_dns_axfr.h,
    libopenvas/hg_add_hosts.h : Added GPL header.
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@2186 423fd1db-d629-0410-8442-d21db03e70f4

--- libopenvas_hg/test.c
+++ libopenvas_hg/test.c
@@ -1,7 +1,26 @@
+/*
+ * Copyright (C) 1999 Renaud Deraison
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <includes.h>
 #include "hosts_gatherer.h"
-/*
- * 
+
+/**
+ * @file
  * This simple program compiles when you link it against
  * the following shared libraries :
  *
@@ -10,8 +29,8 @@
  *	-lopenvas
  *
  * Its purpose is to demonstrate how to use the lib hosts_gatherer
- *
  */
+
 extern int optind;
 int main(int argc, char * argv[])
 {
