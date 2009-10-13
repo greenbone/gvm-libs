2c5afdcde4f9c9fb0e1f6d53fbf7f859de4bd72d
Matthew Mundell <matthew.mundell@greenbone.net>
2009-10-13 15:58:12 +0000

    * include/nvt_categories.h (ACT_UNKNOWN, ACT_STRING_INIT) 	(ACT_STRING_SCANNER, ACT_STRING_SETTINGS, ACT_STRING_GATHER_INFO) 	(ACT_STRING_ATTACK, ACT_STRING_MIXED_ATTACK) 	(ACT_STRING_DESTRUCTIVE_ATTACK, ACT_STRING_DENIAL, ACT_STRING_KILL_HOST) 	(ACT_STRING_FLOOD, ACT_STRING_END, ACT_STRING_UNKNOWN) 	(ACT_STRING_LIST_ALL): New defines.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@5515 423fd1db-d629-0410-8442-d21db03e70f4

--- include/nvt_categories.h
+++ include/nvt_categories.h
@@ -44,6 +44,7 @@
 /** First plugins actions type. */
 #define ACT_FIRST               ACT_INIT
 
+#define ACT_UNKNOWN             11
 #define ACT_END                 10
 #define ACT_FLOOD               9
 #define ACT_KILL_HOST           8
@@ -55,5 +56,31 @@
 #define ACT_SETTINGS            2
 #define ACT_SCANNER             1
 #define ACT_INIT                0
+
+#define ACT_STRING_INIT                "init"
+#define ACT_STRING_SCANNER             "scanner"
+#define ACT_STRING_SETTINGS            "settings"
+#define ACT_STRING_GATHER_INFO         "infos"
+#define ACT_STRING_ATTACK              "attack"
+#define ACT_STRING_MIXED_ATTACK        "mixed"
+#define ACT_STRING_DESTRUCTIVE_ATTACK  "destructive_attack"
+#define ACT_STRING_DENIAL              "denial"
+#define ACT_STRING_KILL_HOST           "kill_host"
+#define ACT_STRING_FLOOD               "flood"
+#define ACT_STRING_END                 "end"
+#define ACT_STRING_UNKNOWN             "unknown"
+
+#define ACT_STRING_LIST_ALL     ACT_STRING_INIT ,               \
+                                ACT_STRING_SCANNER ,            \
+                                ACT_STRING_SETTINGS ,           \
+                                ACT_STRING_GATHER_INFO ,        \
+                                ACT_STRING_ATTACK ,             \
+                                ACT_STRING_MIXED_ATTACK ,       \
+                                ACT_STRING_DESTRUCTIVE_ATTACK , \
+                                ACT_STRING_DENIAL ,             \
+                                ACT_STRING_KILL_HOST ,          \
+                                ACT_STRING_FLOOD ,              \
+                                ACT_STRING_END ,                \
+                                ACT_STRING_UNKNOWN
 
 #endif /* _NVT_CATEGORIES_H */
