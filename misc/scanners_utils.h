bb28b1528cffb2ce85ed1945d3a598c4a89e0d03
Felix Wolfsteller <felix.wolfsteller@greenbone.net>
2009-12-23 07:07:33 +0000

    * misc/scanners_utils.h: Copied header of implementation file to header file.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@6241 423fd1db-d629-0410-8442-d21db03e70f4

--- misc/scanners_utils.h
+++ misc/scanners_utils.h
@@ -1,6 +1,27 @@
+/* OpenVAS Libraries
+ * Copyright (C) 1998 Renaud Deraison
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * scanners_utils -- scanner-plugins-specific stuff
+ */
+
 #ifndef _OPENVAS_SCANNERS_UTILS_H
 #define _OPENVAS_SCANNERS_UTILS_H
 
 int comm_send_status(struct arglist *, char *, char *, int , int);
 unsigned short *getpts(char *, int *);
+
 #endif
