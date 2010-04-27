579fec9cfcde160edba7ea8b9d3ee8619e11a22c
Felix Wolfsteller <felix.wolfsteller@greenbone.net>
2010-04-27 10:48:10 +0000

    Added near-duplicate code from openvas-administrator / openvas-manager.
    * base/credentials.h, base/credentials.c: New module, moved from
    openvas-administrator/oap module.
    
    * base/CMakeLists.txt: Added header and source files to respective
    lists.
    
    * MANIFEST: Added new files.
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@7440 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ base/credentials.c
@@ -0,0 +1,82 @@
+/* openvas-libraries/base
+ * $Id$
+ * Description: Credential pairs and triples.
+ *
+ * Authors:
+ * Matthew Mundell <matthew.mundell@greenbone.net>
+ * Michael Wiegand <michael.wiegand@intevation.de>
+ * Felix Wolfsteller <felix.wolfsteller@intevation.de>
+ *
+ * Copyright:
+ * Copyright (C) 2010 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or, at your option, any later version as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "credentials.h"
+
+#include "openvas_string.h"
+
+/**
+ * @brief Free credentials.
+ *
+ * Free the members of a credentials pair.
+ *
+ * @param[in]  credentials  Pointer to the credentials.
+ */
+void
+free_credentials (credentials_t* credentials)
+{
+  if (credentials->username)
+    {
+      g_free (credentials->username);
+      credentials->username = NULL;
+    }
+  if (credentials->password)
+    {
+      g_free (credentials->password);
+      credentials->password = NULL;
+    }
+}
+
+/**
+ * @brief Append text to the username of a credential pair.
+ *
+ * @param[in]  credentials  Credentials.
+ * @param[in]  text         The text to append.
+ * @param[in]  length       Length of the text.
+ */
+void
+append_to_credentials_username (credentials_t* credentials,
+                                const char* text,
+                                gsize length)
+{
+  openvas_append_text (&credentials->username, text, length);
+}
+
+/**
+ * @brief Append text to the password of a credential pair.
+ *
+ * @param[in]  credentials  Credentials.
+ * @param[in]  text         The text to append.
+ * @param[in]  length       Length of the text.
+ */
+void
+append_to_credentials_password (credentials_t* credentials,
+                                const char* text,
+                                gsize length)
+{
+  openvas_append_text (&credentials->password, text, length);
+}
---
+++ base/credentials.h
@@ -0,0 +1,57 @@
+/* openvas-libraries/base
+ * $Id$
+ * Description: Credential pairs and triples.
+ *
+ * Authors:
+ * Matthew Mundell <matthew.mundell@greenbone.net>
+ * Michael Wiegand <michael.wiegand@intevation.de>
+ * Felix Wolfsteller <felix.wolfsteller@intevation.de>
+ *
+ * Copyright:
+ * Copyright (C) 2010 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or, at your option, any later version as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENVAS_LIBRARIES_BASE_CREDENTIALS_H
+#define _OPENVAS_LIBRARIES_BASE_CREDENTIALS_H
+
+#include "credentials.h"
+
+#include <glib.h>
+
+/**
+ * @brief A username password pair.
+ */
+typedef struct
+{
+  /*@null@*/ gchar* username; ///< Login name of user.
+  /*@null@*/ gchar* password; ///< Password of user.
+} credentials_t;
+
+void
+free_credentials (credentials_t* credentials);
+
+void
+append_to_credentials_username (credentials_t* credentials,
+                                const char* text,
+                                gsize length);
+
+void
+append_to_credentials_password (credentials_t* credentials,
+                                const char* text,
+                                gsize length);
+
+#endif /* _OPENVAS_LIBRARIES_BASE_CREDENTIALS_H */
