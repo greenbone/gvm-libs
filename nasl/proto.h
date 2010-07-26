df9a118781a4413e50906d2d2ba73137d937880b
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2010-07-26 20:38:09 +0000

    * nasl/nasl_icmp.h, nasl/nasl_ip.h, nasl/nasl_raw.h, nasl/nasl_tcp.h, nasl/nasl_udp.h: Added missing header with author, copyright and license. Since this header file was part of libnasl, the full header of libnasl/nasl/capture_packet.c is used as a typical representative of this module.
    * nasl/proto.h: Added missing header. Copied from ntlmssp.h.
    
    * COPYING: Completed.
    
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@8574 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/proto.h
+++ nasl/proto.h
@@ -1,3 +1,28 @@
+/* OpenVAS
+ *
+ * $Id$
+ * Description: Implementation for NTLMSSP support
+ *
+ * Author:
+ * Preeti Subramanian <spreeti@secpod.com>
+ *
+ * Copyright:
+ * Copyright (c) 2010 Greenbone Networks GmbH, http://www.greenbone.net
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * (or any later version), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <time.h>
 #include "smb.h"
 /*implemented in genrand.c*/
