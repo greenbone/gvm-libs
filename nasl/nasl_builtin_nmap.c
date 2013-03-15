bc2afcea456b3e9654836d7b51efb570fceccfcf
Hani Benhabiles <hani.benhabiles@greenbone.net>
2013-03-15 12:08:00 +0000

    Backport: r15626.
    * nasl/nasl_builtin_nmap.c: Changed license from GPLv2 to GPLv2+
    copyrighted by Greenbone.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-libraries-6.0@15700 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_builtin_nmap.c
+++ nasl/nasl_builtin_nmap.c
@@ -10,7 +10,8 @@
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License version 2,
-* as published by the Free Software Foundation
+* or, at your option, any later version as published by the Free
+* Software Foundation
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,8 +21,6 @@
 * You should have received a copy of the GNU General Public License
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-*
 */
 
 /**
