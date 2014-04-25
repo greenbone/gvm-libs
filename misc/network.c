81ab10c3a163c6798e6495fbdf149ae5694f5f0b
Hani Benhabiles <hani.benhabiles@greenbone.net>
2014-04-25 08:40:56 +0000

    * misc/network.c (socket_get_ssl_ciphersuite): Refix sign issue. API in GnuTLS 2.x takes a char * while it takes an unsigned char * in GnuTLS 3.x. Cast to void pointer to be compatible with both.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@19328 423fd1db-d629-0410-8442-d21db03e70f4

--- misc/network.c
+++ misc/network.c
@@ -974,7 +974,8 @@ socket_get_ssl_ciphersuite (int fd)
   kx = gnutls_kx_get (session);
   cipher = gnutls_cipher_get (session);
   mac = gnutls_mac_get (session);
-  while (gnutls_cipher_suite_info (idx, cs_id, &kx2, &cipher2, &mac2, NULL))
+  while (gnutls_cipher_suite_info (idx, (void *) cs_id, &kx2, &cipher2, &mac2,
+                                   NULL))
     {
       if (kx == kx2 && cipher == cipher2 && mac == mac2)
         return cs_id[0] + cs_id[1];
