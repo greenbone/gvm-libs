c10a8cb257f118960b867397b8cc543574e0f439
Michael Wiegand <michael.wiegand@greenbone.net>
2014-09-01 10:33:53 +0000

    * nasl/CMakeLists.txt, nasl/nasl_builtin_nmap.c,   nasl/nasl_builtin_plugins.h, nasl/nasl_cert.c, nasl/nasl_cert.h,   nasl/nasl_isotime.c, nasl/nasl_isotime.h, nasl/nasl_signature.c,   nasl/nasl_signature.h, nasl/nasl_smb.c, nasl/nasl_smb.h,   nasl/nasl_ssh.c, nasl/nasl_ssh.h, nasl/nasl_wmi.c, nasl/nasl_wmi.h,   nasl/ntlmssp.c, nasl/ntlmssp.h, nasl/openvas_smb_interface.h,   nasl/openvas_wmi_interface.h, nasl/proto.h, nasl/smb_interface_stub.c,   nasl/wmi_interface_stub.c, nasl/tests/Makefile,   nasl/tests/signed.nasl, nasl/tests/test_blowfish.nasl,   nasl/tests/test_bn.nasl, nasl/tests/test_dh.nasl,   nasl/tests/test_dsa.nasl, nasl/tests/test_hexstr.nasl,   nasl/tests/test_isotime.nasl, nasl/tests/test_md.nasl,   nasl/tests/test_privkey.nasl, nasl/tests/test_rsa.nasl,   nasl/tests/test_socket.nasl, nasl/tests/testsuiteinit.nasl,   nasl/tests/testsuitesummary.nasl: Ensure the GPLv2+ license notice   uses the paragraph suggested by the Free Software Foundation.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@20222 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/openvas_wmi_interface.h
+++ nasl/openvas_wmi_interface.h
@@ -10,9 +10,10 @@
  * Copyright:
  * Copyright (c) 2009 Greenbone Networks GmbH, http://www.greenbone.net
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * (or any later version), as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
