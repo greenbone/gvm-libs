f0e1dd8378158870287d24ffb059c32df64f39b8
Michael Wiegand <michael.wiegand@greenbone.net>
2014-09-05 06:27:43 +0000

    * nasl/nasl_builtin_find_service.c: Add proper copyright and license   notice header.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@20271 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_builtin_find_service.c
+++ nasl/nasl_builtin_find_service.c
@@ -1,7 +1,20 @@
 /*
  * Find service
  *
- * This plugin is released under the GPL
+ * Copyright (C) 2002 Renaud Deraison <deraison@cvs.nessus.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #define SMART_TCP_RW
 /* #define DEBUG */
