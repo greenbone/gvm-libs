greenbone/gvm-libs

commit 3f471e96fcdc01e567de011e2c0f051c61ee2503

* nasl/exec.c (cell2str): Replace malloc() by g_malloc() and drop now useless test for NULL.
* nasl/nasl_tree.c (alloc_tree_cell): Replace malloc() by
g_malloc() and drop now useless test for NULL.

* nasl/nasl_cert.c (parse_dn_part_for_CN): Replace malloc() by
g_malloc() and drop now useless test for NULL.

* base/openvas_networking.c (openvas_source_addr_str,
openvas_source_addr6_str): Replace malloc() by g_malloc() and drop
now useless test for NULL.




git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@20560 423fd1db-d629-0410-8442-d21db03e70f4

nasl/nasl_cert.c
@@ -330,11 +330,8 @@ parse_dn_part_for_CN (const char *string, char **r_value)
         return NULL; /* No or odd number of digits. */
       n /= 2;
       if (found)
-        {
-          *r_value = p = malloc (n+1);
-          if (!p)
-            return NULL; /* Out of core.  */
-        }
+        *r_value = g_malloc0 (n+1);
+
       for (s1=string; n; s1 += 2, n--, p++)
         {
           if (found)
@@ -377,11 +374,8 @@ parse_dn_part_for_CN (const char *string, char **r_value)
         }
 
       if (found)
-        {
-          *r_value = p = malloc (n+1);
-          if (!p)
-            return NULL;
-        }
+        *r_value = g_malloc0 (n+1);
+
       for (s=string; n; s++, n--)
         {
           if (*s == '\\')

base/openvas_networking.c
@@ -185,10 +185,7 @@ openvas_source_addr_as_addr6 (struct in6_addr *addr6)
 char *
 openvas_source_addr_str ()
 {
-  char *str = malloc (INET_ADDRSTRLEN);
-
-  if (str == NULL)
-    return NULL;
+  char *str = g_malloc0 (INET_ADDRSTRLEN);
 
   inet_ntop (AF_INET, &global_source_addr.s_addr, str, INET_ADDRSTRLEN);
   return str;
@@ -202,10 +199,7 @@ openvas_source_addr_str ()
 char *
 openvas_source_addr6_str ()
 {
-  char *str = malloc (INET6_ADDRSTRLEN);
-
-  if (str == NULL)
-    return NULL;
+  char *str = g_malloc0 (INET6_ADDRSTRLEN);
 
   inet_ntop (AF_INET6, &global_source_addr6, str, INET6_ADDRSTRLEN);
   return str;

nasl/exec.c
@@ -158,9 +158,8 @@ cell2str (lex_ctxt * lexic, tree_cell * c)
   switch (c->type)
     {
     case CONST_INT:
-      p = malloc (16);
-      if (p != NULL)
-        snprintf (p, 16, "%d", c->x.i_val);     /* RATS: ignore */
+      p = g_malloc0 (16);
+      snprintf (p, 16, "%d", c->x.i_val);
       return p;
 
     case CONST_STR:

nasl/nasl_tree.c
@@ -36,14 +36,9 @@
 tree_cell *
 alloc_tree_cell (int lnb, char *s)
 {
-  tree_cell *p = malloc (sizeof (tree_cell));
+  tree_cell *p = g_malloc0 (sizeof (tree_cell));
   int i;
 
-  if (p == NULL)
-    {
-      perror ("malloc");
-      abort ();
-    }
   p->type = 0;
   p->size = 0;
   p->line_nb = lnb;

commit f57d0ac76f26d8e4139c8aa000c0c2ab52b45078

* nasl/nasl_cert.c (parse_dn_part_for_CN): Set p pointer properly. Fixes scanner segfault introduced by r20560.
git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@20655 423fd1db-d629-0410-8442-d21db03e70f4

nasl/nasl_cert.c
@@ -329,7 +329,7 @@ parse_dn_part_for_CN (const char *string, char **r_value)
         return NULL; /* No or odd number of digits. */
       n /= 2;
       if (found)
-        *r_value = g_malloc0 (n+1);
+        *r_value = p = g_malloc0 (n + 1);
 
       for (s1=string; n; s1 += 2, n--, p++)
         {
@@ -373,7 +373,7 @@ parse_dn_part_for_CN (const char *string, char **r_value)
         }
 
       if (found)
-        *r_value = g_malloc0 (n+1);
+        *r_value = p = g_malloc0 (n + 1);
 
       for (s=string; n; s++, n--)
         {