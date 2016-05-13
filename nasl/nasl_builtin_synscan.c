23253af6f507040f6f0dbf4d69f3922e48b79ba4
Michael Wiegand <michael.wiegand@greenbone.net>
2016-05-13 07:47:58 +0000

    * nasl/nasl_builtin_synscan.c (plugin_run_synscan): Fix identation to make it clear that the statement is not supposed to be guarded by the preceding 'if' clause. The 'if' clause was split into two lines in r6439 (then openvas-scanner/cnvts/synscan/synscan.c), so it is assumed that it was never intended to guard the statement.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@25293 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_builtin_synscan.c
+++ nasl/nasl_builtin_synscan.c
@@ -803,7 +803,7 @@ plugin_run_synscan (lex_ctxt * lexic)
   if (islocalhost (dst))
     return NULL;
 
-    rtt = htonl (1 << 28);
+  rtt = htonl (1 << 28);
 
 #ifdef DEBUG
   printf ("RTT = 0x%.8x\n", ntohl (rtt));
