de09cbfcafde141a6b1b3c587d2b4cc8f8385323
Matthew Mundell <matthew.mundell@greenbone.net>
2016-07-20 18:33:11 +0000

    * omp/omp.c (proctitle_init): Init __progname and _progname_full because 	something in this function is nuking them, and they're used by the syslog 	functions.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@25807 423fd1db-d629-0410-8442-d21db03e70f4

--- misc/openvas_proctitle.c
+++ misc/openvas_proctitle.c
@@ -29,6 +29,8 @@
 
 #include "openvas_proctitle.h"
 
+extern const char *__progname;
+extern const char *__progname_full;
 static int argv_len;
 static char **old_argv;
 extern char **environ;
@@ -45,9 +47,14 @@ proctitle_init (int argc, char **argv)
 {
   int i = 0;
   char **envp = environ;
+  char *new_progname, *new_progname_full;
 
   if (argv == NULL)
     return;
+
+  new_progname = strdup (__progname);
+  new_progname_full = strdup (__progname_full);
+
   /* Move environ to new memory, to be able to reuse older one. */
   while (envp[i]) i++;
   environ = g_malloc0 (sizeof(char *) * (i + 1));
@@ -63,6 +70,11 @@ proctitle_init (int argc, char **argv)
     argv_len = envp[i-1] + strlen(envp[i-1]) - old_argv[0];
   else
     argv_len = old_argv[argc-1] + strlen(old_argv[argc-1]) - old_argv[0];
+
+  /* Seems like these are in the moved environment, so reset them.  Idea from
+   * proctitle.cpp in KDE libs.  */
+  __progname = new_progname;
+  __progname_full = new_progname_full;
 }
 
 /**
