da728ee0141893f99c6b59dc3f1bd1f7b1a33e2e
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2017-01-01 20:06:31 +0000

    * base/drop_privileges.c, base/drop_privileges.h: New. Moved here from openvas-libraries.
    * base/CMakeLists.txt: Handle new module "drop_privileges".
    
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gvm-libs@27081 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ base/drop_privileges.h
@@ -0,0 +1,49 @@
+/* gvm-libs/base
+ * $Id$
+ * Description: Privilege dropping header file.
+ *
+ * Authors:
+ * Felix Wolfsteller <felix.wolfsteller@greenbone.net>
+ * based on work by Michael Wiegand <michael.wiegand@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2010 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _GVM_DROP_PRIVILEGES_H
+#define _GVM_DROP_PRIVILEGES_H
+
+#include <glib.h>
+
+/**
+ * @brief The GQuark for privilege dropping errors.
+ */
+#define GVM_DROP_PRIVILEGES g_quark_from_static_string ("gvm-drop-privileges-error-quark")
+
+/* Definitions of the return codes. */
+#define GVM_DROP_PRIVILEGES_ERROR_ALREADY_SET -1
+
+#define GVM_DROP_PRIVILEGES_OK 0
+#define GVM_DROP_PRIVILEGES_FAIL_NOT_ROOT 1
+#define GVM_DROP_PRIVILEGES_FAIL_UNKNOWN_USER 2
+#define GVM_DROP_PRIVILEGES_FAIL_DROP_GID 3
+#define GVM_DROP_PRIVILEGES_FAIL_DROP_UID 4
+#define GVM_DROP_PRIVILEGES_FAIL_SUPPLEMENTARY 5
+
+int drop_privileges (gchar * username, GError ** error);
+
+#endif
