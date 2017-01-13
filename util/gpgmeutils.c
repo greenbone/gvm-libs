6f9e4e725cdfa7eb33ce7edf53d64de8a503e72d
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2017-01-13 18:35:54 +0000

    * util/gpgmeutils.c, util/gpgmeutils.h: New. Moved here from openvas-libraries. Reduced to only offer gvm_init_gpgme_ctx_from_dir().
    * util/CMakeLists.txt: Handle new module "gpgmeutils". Check for
    gpgme library.
    
    * INSTALL: Add gpgme library as dependency.
    
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gvm-libs@27213 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ util/gpgmeutils.c
@@ -0,0 +1,174 @@
+/* gvm-libs/util
+ * $Id$
+ * Description: GPGME utilities.
+ *
+ * Authors:
+ * Bernhard Herzog <bernhard.herzog@intevation.de>
+ * Werner Koch <wk@gnupg.org>
+ *
+ * Copyright:
+ * Copyright (C) 2009,2013 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * @file gpgmeutils.c
+ * @brief GPGME utilities.
+ */
+
+#include <locale.h>             /* for LC_CTYPE  */
+#include <unistd.h>             /* for F_OK */
+#include <sys/stat.h>           /* for mkdir */
+#include <errno.h>              /* for ENOENT */
+
+#include "gpgmeutils.h"
+
+#undef G_LOG_DOMAIN
+/**
+ * @brief GLib log domain.
+ */
+#define G_LOG_DOMAIN "util gpgme"
+
+/**
+ * @brief Log function with extra gpg-error style output
+ *
+ * If @ref err is not 0, the appropriate error string is appended to
+ * the output.  It takes care to only add the error source string if
+ * it makes sense.
+ *
+ * @todo: Make this a global function.  There is already a copy in the
+ *        manager
+ *
+ * @param level  The GLib style log level
+ * @param err    An gpg-error value or 0
+ * @param fmt    The printf style format string, followed by its
+ *                arguments.
+ *
+ */
+static void
+log_gpgme (GLogLevelFlags level, gpg_error_t err, const char *fmt, ...)
+{
+  va_list arg_ptr;
+  char *msg;
+
+  va_start (arg_ptr, fmt);
+  msg = g_strdup_vprintf (fmt, arg_ptr);
+  va_end (arg_ptr);
+  if (err && gpg_err_source (err) != GPG_ERR_SOURCE_ANY
+          && gpg_err_source (err))
+    g_log (G_LOG_DOMAIN, level, "%s: %s <%s>",
+           msg, gpg_strerror (err), gpg_strsource (err));
+  else if (err)
+    g_log (G_LOG_DOMAIN, level, "%s: %s",
+           msg, gpg_strerror (err));
+  else
+    g_log (G_LOG_DOMAIN, level, "%s",
+           msg);
+  g_free (msg);
+}
+
+/**
+ * @brief Returns a new gpgme context.
+ *
+ * Inits a gpgme context with the custom gpg directory, protocol
+ * version etc. Returns the context or NULL if an error occurred.
+ * This function also does an gpgme initialization the first time it
+ * is called.
+ *
+ * @param dir  Directory to use for gpg
+ *
+ * @return The gpgme_ctx_t to the context or NULL if an error occurred.
+ */
+gpgme_ctx_t
+gvm_init_gpgme_ctx_from_dir (const gchar *dir)
+{
+  static int initialized;
+  gpgme_error_t err;
+  gpgme_ctx_t ctx;
+
+  /* Initialize GPGME the first time we are called.  This is a
+     failsafe mode; it would be better to initialize GPGME early at
+     process startup instead of this on-the-fly method; however in
+     this non-threaded system; this is an easier way for a library.
+     We allow to initialize until a valid gpgme or a gpg backend has
+     been found.  */
+  if (!initialized)
+    {
+      gpgme_engine_info_t info;
+
+      if (!gpgme_check_version (NULL))
+        {
+          g_critical ("gpgme library could not be initialized.");
+          return NULL;
+        }
+      gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
+#   ifdef LC_MESSAGES
+      gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
+#   endif
+
+#ifndef NDEBUG
+      g_message ("Setting GnuPG dir to '%s'", dir);
+#endif
+      err = 0;
+      if (access (dir, F_OK))
+        {
+          err = gpg_error_from_syserror ();
+
+          if (errno == ENOENT)
+            /* directory does not exists. try to create it */
+            if (mkdir (dir, 0700) == 0)
+              {
+#ifndef NDEBUG
+                g_message ("Created GnuPG dir '%s'", dir);
+#endif
+                err = 0;
+              }
+        }
+
+      if (!err)
+        err = gpgme_set_engine_info (GPGME_PROTOCOL_OpenPGP, NULL, dir);
+
+      if (err)
+        {
+          log_gpgme (G_LOG_LEVEL_WARNING, err, "Setting GnuPG dir failed");
+          return NULL;
+        }
+
+      /* Show the OpenPGP engine version.  */
+      if (!gpgme_get_engine_info (&info))
+        {
+          while (info && info->protocol != GPGME_PROTOCOL_OpenPGP)
+            info = info->next;
+        }
+      else
+        info = NULL;
+#ifndef NDEBUG
+      g_message ("Using OpenPGP engine version '%s'",
+                 info && info->version? info->version: "[?]");
+#endif
+
+      /* Everything is fine.  */
+      initialized = 1;
+    }
+
+  /* Allocate the context.  */
+  ctx = NULL;
+  err = gpgme_new (&ctx);
+  if (err)
+    log_gpgme (G_LOG_LEVEL_WARNING, err, "Creating GPGME context failed");
+
+  return ctx;
+}
---
+++ util/gpgmeutils.h
@@ -0,0 +1,41 @@
+/* gvm-libs/util
+ * $Id$
+ * Description: Definitions for GPGME utilities.
+ *
+ * Authors:
+ * Werner Koch <wk@gnupg.org>
+ *
+ * Copyright:
+ * Copyright (C) 2013 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * @file gpgmeutils.h
+ * @brief Protos and data structures for GPGME utilities.
+ *
+ * This file contains the protos for \ref gpgmeutils.c
+ */
+
+#ifndef _GVM_GPGMEUTILS_H
+#define _GVM_GPGMEUTILS_H
+
+#include <glib.h>  /* for gchar */
+#include <gpgme.h> /* for gpgme_ctx_t */
+
+gpgme_ctx_t gvm_init_gpgme_ctx_from_dir (const gchar *);
+
+#endif /*_GVM_GPGMEUTILS_H*/
