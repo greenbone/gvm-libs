4d69c386104cefa686bb4b79604bccec4530ae5f
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2017-02-10 20:19:49 +0000

    * base/proctitle.c, base/proctitle.h: New. Moved here from openvas-libraries.
    * base/CMakeLists.txt: Handle new module "proctitle".
    
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gvm-libs@27564 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ base/proctitle.h
@@ -0,0 +1,36 @@
+/* gvm-libs/base
+ * $Id$
+ * Description: API for process title setting.
+ *
+ * Authors:
+ * Hani Benhabiles <hani.benhabiles@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2014 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _GVM_PROCTITLE_H
+#define _GVM__PROCTITLE_H
+
+void
+proctitle_init (int, char **);
+
+void
+proctitle_set (const char *, ...);
+
+#endif /* not _GVM_PROCTITLE_H */
+
