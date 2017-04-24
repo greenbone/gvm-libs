be42616f4b283dec357f4d151b845b5a50c98c87
Hani Benhabiles <hani.benhabiles@greenbone.net>
2017-04-24 20:45:17 +0000

    * misc/network.c (open_socket), nasl/nasl_isotime.c (ISOTIME_SIZE): Fix compilation with GCC 7.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-libraries-9.0@28274 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_isotime.c
+++ nasl/nasl_isotime.c
@@ -75,7 +75,7 @@
 
 /* The type used to represent the time here is a string with a fixed
    length.  */
-#define ISOTIME_SIZE 16
+#define ISOTIME_SIZE 32
 typedef char my_isotime_t[ISOTIME_SIZE];
 
 /* Correction used to map to real Julian days. */
