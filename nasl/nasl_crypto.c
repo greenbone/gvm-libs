82e20d88979a57c4fd4520b045a69a138ce9fa98
Hani Benhabiles <hani.benhabiles@greenbone.net>
2017-05-19 14:57:51 +0000

    Backport r28438.
    * nasl/nasl_crypto.c (nasl_nt_owf_gen): Fix memory leak. Improve code
    style. Fix calculation for inputs of more than 64 characters.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-libraries-9.0@28439 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_crypto.c
+++ nasl/nasl_crypto.c
@@ -794,17 +794,19 @@ tree_cell *
 nasl_nt_owf_gen (lex_ctxt * lexic)
 {
   char *pass = get_str_var_by_num (lexic, 0);
-  int pass_len = get_var_size_by_num (lexic, 0);
-  glong items_written;
-  gunichar2 *upass = g_utf8_to_utf16 (pass,-1,NULL, &items_written,NULL);
-  if (pass_len < 0 || pass == NULL)
+  gunichar2 *upass;
+  glong upass_len;
+  tree_cell *ret;
+
+  if (!pass)
     {
       nasl_perror (lexic, "Syntax : nt_owf_gen(password:<p>)\n");
       return NULL;
     }
-
-  return nasl_gcrypt_hash (lexic, GCRY_MD_MD4, upass,
-                           items_written * 2 > 128 ? 128 : items_written * 2, NULL, 0);
+  upass = g_utf8_to_utf16 (pass, -1, NULL, &upass_len, NULL);
+  ret = nasl_gcrypt_hash (lexic, GCRY_MD_MD4, upass, upass_len * 2, NULL, 0);
+  g_free (upass);
+  return ret;
 }
 
 tree_cell *
