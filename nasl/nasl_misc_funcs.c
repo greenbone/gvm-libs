6420b7c15c7ff74ffc92cbc40421e46f7f3774e9
Hani Benhabiles <hani.benhabiles@greenbone.net>
2017-07-12 01:25:51 +0000

    Backport r28779.
    * nasl/nasl_func.h (nasl_func): Remove nb_named_args element.
    
    * nasl/nasl_func.c (insert_nasl_func, nasl_func_call, free_func): Adjust
    for nasl_func change.
    
    * nasl/nasl_init.c (init_nasl_library): Don't check for named args
    ordering.
    
    * nasl/nasl_misc_funcs.c (nasl_func_named_args, nasl_func_has_arg):
    Adjust for nasl_func change.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-libraries-9.0@29039 423fd1db-d629-0410-8442-d21db03e70f4

--- nasl/nasl_misc_funcs.c
+++ nasl/nasl_misc_funcs.c
@@ -630,7 +630,7 @@ nasl_func_named_args (lex_ctxt * lexic)
 {
   nasl_func *f;
   char *s;
-  int i;
+  int i, nb;
   tree_cell *retc;
   nasl_array *a;
   anon_nasl_var v;
@@ -655,7 +655,8 @@ nasl_func_named_args (lex_ctxt * lexic)
   memset (&v, 0, sizeof (v));
   v.var_type = VAR2_STRING;
 
-  for (i = 0; i < f->nb_named_args; i++)
+  nb = g_strv_length (f->args_names);
+  for (i = 0; i < nb; i++)
     {
       v.v.v_str.s_val = (unsigned char *) f->args_names[i];
       v.v.v_str.s_siz = strlen (f->args_names[i]);
@@ -720,6 +721,8 @@ nasl_func_has_arg (lex_ctxt * lexic)
   vt = get_var_type_by_num (lexic, 1);
   switch (vt)
     {
+    int nb;
+
     case VAR2_INT:
       i = get_int_var_by_num (lexic, 1, -1);
       if (i >= 0 && i < f->nb_unnamed_args)
@@ -729,7 +732,8 @@ nasl_func_has_arg (lex_ctxt * lexic)
     case VAR2_STRING:
     case VAR2_DATA:
       s = get_str_var_by_num (lexic, 1);
-      for (i = 0; i < f->nb_named_args && !flag; i++)
+      nb = g_strv_length (f->args_names);
+      for (i = 0; i < nb && !flag; i++)
         if (strcmp (s, f->args_names[i]) == 0)
           flag = 1;
       break;
