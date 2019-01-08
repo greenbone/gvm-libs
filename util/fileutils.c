2ab9564fe3df617e6f37e8fca77ba3cba18f6c3b
Timo Pollmeier <timo.pollmeier@greenbone.net>
2019-01-08 17:49:00 +0100

    Replace ASCII control chars in export filenames
    ASCII whitespace and control characters are now replaced with '_' in
    gvm_export_file_name.

--- util/fileutils.c
+++ util/fileutils.c
@@ -324,6 +324,8 @@ gvm_export_file_name (const char *fname_format, const char *username,
             format_state = 1;
           else if (*fname_point == '"')
             g_string_append (file_name_buf, "\\\"");
+          else if (*fname_point <= ' ')
+            g_string_append_c (file_name_buf, '_');
           else
             g_string_append_c (file_name_buf, *fname_point);
         }
