2d4df60be6905fcdef0d4078f8a0f81686147acd
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2019-05-05 11:26:19 +0200

    Fix memory management bug.

--- util/kb.c
+++ util/kb.c
@@ -1293,9 +1293,9 @@ redis_add_nvt (kb_t kb, const nvti_t *nvt, const char *filename)
   if (!nvt || !filename)
     return -1;
 
-  cves = nvti_refs (nvt, "cve", "", 0) ?: "";
-  bids = nvti_refs (nvt, "bid", "", 0) ?: "";
-  xrefs = nvti_refs (nvt, NULL, "cve,bid", 1) ?: "";
+  cves = nvti_refs (nvt, "cve", "", 0);
+  bids = nvti_refs (nvt, "bid", "", 0);
+  xrefs = nvti_refs (nvt, NULL, "cve,bid", 1);
 
   kbr = redis_kb (kb);
   rep = redis_cmd (
@@ -1303,8 +1303,8 @@ redis_add_nvt (kb_t kb, const nvti_t *nvt, const char *filename)
     nvti_oid (nvt), filename, nvti_required_keys (nvt) ?: "",
     nvti_mandatory_keys (nvt) ?: "", nvti_excluded_keys (nvt) ?: "",
     nvti_required_udp_ports (nvt) ?: "", nvti_required_ports (nvt) ?: "",
-    nvti_dependencies (nvt) ?: "", nvti_tag (nvt) ?: "", cves, bids, xrefs,
-    nvti_category (nvt),
+    nvti_dependencies (nvt) ?: "", nvti_tag (nvt) ?: "", cves ?: "",
+    bids ?: "", xrefs ?: "", nvti_category (nvt),
     nvti_timeout (nvt), nvti_family (nvt), nvti_name (nvt));
   g_free (cves);
   g_free (bids);
