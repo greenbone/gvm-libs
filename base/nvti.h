b570a5c258ef4fee08d4152d04d6338e97d7f490
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2019-07-31 22:18:50 +0200

    Make the actual nvti struct internal.
    This enforces to use the intended API. It
    can not be bypassed anymore by accessing the elements directly.

--- base/nvti.h
+++ base/nvti.h
@@ -68,36 +68,8 @@ typedef struct vtref vtref_t;
 
 /**
  * @brief The structure of a information record that corresponds to a NVT.
- *
- * The elements of this structure should never be accessed directly.
- * Only the functions corresponding to this module should be used.
  */
-typedef struct nvti
-{
-  gchar *oid;  /**< @brief Object ID */
-  gchar *name; /**< @brief The name */
-
-  gchar *tag;       /**< @brief List of tags attached to this NVT */
-  gchar *cvss_base; /**< @brief CVSS base score for this NVT. */
-
-  gchar *dependencies;   /**< @brief List of dependencies of this NVT */
-  gchar *required_keys;  /**< @brief List of required KB keys of this NVT */
-  gchar *mandatory_keys; /**< @brief List of mandatory KB keys of this NVT */
-  gchar *excluded_keys;  /**< @brief List of excluded KB keys of this NVT */
-  gchar *required_ports; /**< @brief List of required ports of this NVT */
-  gchar
-    *required_udp_ports; /**< @brief List of required UDP ports of this NVT*/
-
-  gchar *qod_type; /**< @brief Quality of detection type */
-
-  GSList *refs;  /**< @brief Collection of VT references */
-  GSList *prefs; /**< @brief Collection of NVT preferences */
-
-  // The following are not settled yet.
-  gint timeout;  /**< @brief Default timeout time for this NVT */
-  gint category; /**< @brief The category, this NVT belongs to */
-  gchar *family; /**< @brief Family the NVT belongs to */
-} nvti_t;
+typedef struct nvti nvti_t;
 
 vtref_t *
 vtref_new (const gchar *, const gchar *, const gchar *);
