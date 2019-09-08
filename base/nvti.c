d707048219ae79f8125d2998e0d2c3e3f431a92e
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2019-09-08 10:28:06 +0200

    Rework nvti_add_tag.
    Fix the types and condense the setting of the tag.

--- base/nvti.c
+++ base/nvti.c
@@ -1060,8 +1060,7 @@ nvti_set_solution_type (nvti_t *n, const gchar *solution_type)
 /**
  * @brief Add a tag to the NVT tags.
  *
- * @param n The NVT Info structure.
- *
+ * @param n     The NVT Info structure.
  * @param name  The tag name. A copy will be created from this.
  * @param value The tag value. A copy will be created from this.
  *
@@ -1070,7 +1069,7 @@ nvti_set_solution_type (nvti_t *n, const gchar *solution_type)
 int
 nvti_add_tag (nvti_t *n, const gchar *name, const gchar *value)
 {
-  char *newtag;
+  gchar *newtag;
 
   if (!n)
     return (-1);
@@ -1085,14 +1084,10 @@ nvti_add_tag (nvti_t *n, const gchar *name, const gchar *value)
     {
       newtag = g_strconcat (n->tag, "|", name, "=", value, NULL);
       g_free (n->tag);
+      n->tag = newtag;
     }
   else
-    newtag = g_strconcat (name, "=", value, NULL);
-
-  if (newtag && newtag[0])
-    n->tag = newtag;
-  else
-    n->tag = NULL;
+    n->tag = g_strconcat (name, "=", value, NULL);
 
   return (0);
 }
