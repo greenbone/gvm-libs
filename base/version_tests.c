ef631cd7322400ee77c146d871116d13448128fe
Björn Ricks <bjoern.ricks@greenbone.net>
2019-11-28 16:05:57 +0100

    Use a more descriptive test name

--- base/version_tests.c
+++ base/version_tests.c
@@ -30,7 +30,7 @@ AfterEach (version)
 {
 }
 
-Ensure (version, compare_version)
+Ensure (version, gvm_libs_versions_returns_correct_version)
 {
   assert_that (strcmp (gvm_libs_version (), GVM_LIBS_VERSION) == 0)
 }
@@ -42,7 +42,8 @@ main (int argc, char **argv)
 
   suite = create_test_suite ();
 
-  add_test_with_context (suite, version, compare_version);
+  add_test_with_context (suite, version,
+                         gvm_libs_versions_returns_correct_version);
 
   if (argc > 1)
     return run_single_test (suite, argv[1], create_text_reporter ());
