f0fb4305f2c97ea08caa01f4dd1f6a914a17c345
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-06-09 10:17:44 +0200

    Move in_cksum to util file

--- boreas/ping.c
+++ boreas/ping.c
@@ -20,6 +20,7 @@
 #include "ping.h"
 
 #include "../base/networking.h" /* for gvm_routethrough() */
+#include "util.h"
 
 #include <arpa/inet.h>
 #include <errno.h>
@@ -47,45 +48,6 @@ struct arp_hdr
   uint8_t target_mac[6];
   uint8_t target_ip[4];
 };
-
-/**
- * @brief Checksum calculation.
- *
- * From W.Richard Stevens "UNIX NETWORK PROGRAMMING" book. libfree/in_cksum.c
- * TODO: Section 8.7 of TCPv2 has more efficient implementation
- **/
-static uint16_t
-in_cksum (uint16_t *addr, int len)
-{
-  int nleft = len;
-  uint32_t sum = 0;
-  uint16_t *w = addr;
-  uint16_t answer = 0;
-
-  /*
-   * Our algorithm is simple, using a 32 bit accumulator (sum), we add
-   * sequential 16 bit words to it, and at the end, fold back all the
-   * carry bits from the top 16 bits into the lower 16 bits.
-   */
-  while (nleft > 1)
-    {
-      sum += *w++;
-      nleft -= 2;
-    }
-
-  /* mop up an odd byte, if necessary */
-  if (nleft == 1)
-    {
-      *(unsigned char *) (&answer) = *(unsigned char *) w;
-      sum += answer;
-    }
-
-  /* add back carry outs from top 16 bits to low 16 bits */
-  sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
-  sum += (sum >> 16);                 /* add carry */
-  answer = ~sum;                      /* truncate to 16 bits */
-  return (answer);
-}
 
 /**
  * @brief Send icmp ping.
