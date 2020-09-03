f20c13e769c8325d7df9240df99bb821a2f41933
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-09-03 09:49:35 +0200

    Do not unnecessarily wait for replies
    If already all hosts in the target list were
    identified as alive we do not need to wait for
    ping replies anymore.

--- boreas/alivedetection.h
+++ boreas/alivedetection.h
@@ -28,7 +28,7 @@
 /* to how many hosts are packets send to at a time. value <= 0 for no rate limit
  */
 #define BURST 100
-/* how long (in msec) to wait until new BURST of packets is send */
+/* how long (in microseconds) to wait until new BURST of packets is send */
 #define BURST_TIMEOUT 100000
 /* how tong (in sec) to wait for replies after last packet was sent */
 #define WAIT_FOR_REPLIES_TIMEOUT 1
