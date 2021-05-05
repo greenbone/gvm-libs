5eebabf615e01b8525fdd2f04aa570d86a642d4f
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-05-05 05:50:34 -0500

    Add print_results member to the scanner struct.
    This is used to indicate if results must be print in the stdout
    Before, if there is no main_kb to store the results, the results were just printed
    in the stdout. Now we can perform an alive test without printing and without storing results in the kb.

--- boreas/alivedetection.h
+++ boreas/alivedetection.h
@@ -74,6 +74,8 @@ struct scanner
   pcap_t *pcap_handle;
   hosts_data_t *hosts_data;
   scan_restrictions_t *scan_restrictions;
+  /* 0 do not print in stdout, 1 print in stdout used for cmd line cli. */
+  int print_results;
 };
 
 typedef struct scanner scanner_t;
