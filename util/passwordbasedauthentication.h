7ce7ebf6738202239a4b07d8bf846e19df970c65
ArnoStiefvater <arno.stiefvater@greenbone.net>
2021-05-31 15:06:46 +0200

    Fix format

--- util/passwordbasedauthentication.h
+++ util/passwordbasedauthentication.h
@@ -18,7 +18,7 @@
 #ifndef _GVM_PASSWORDBASEDAUTHENTICATION_H
 #define _GVM_PASSWORDBASEDAUTHENTICATION_H
 
-/* max amount of applied pepper */ 
+/* max amount of applied pepper */
 #define MAX_PEPPER_SIZE 4
 /* is used when count is 0 on init*/
 #define COUNT_DEFAULT 20000
@@ -27,7 +27,7 @@
 
 /**
  *
- * PBASettings is used by pba_hash to control SALT, HASH function and 
+ * PBASettings is used by pba_hash to control SALT, HASH function and
  * computional costs.
  *
  * */
@@ -35,9 +35,9 @@ struct PBASettings
 {
   char pepper[MAX_PEPPER_SIZE]; /* is statically applied to the random salt */
   unsigned int count; /* controls the computational cost of the hash */
-  char *prefix; /* controls which hash function will be used */
+  char *prefix;       /* controls which hash function will be used */
 };
-/** 
+/**
  * Intitializes PBASettings with given PEPPER, PREFIX, COUNT.
  *
  * PEPPER_SIZE must be lower or equal MAX_PEPPER_SIZE when PEPPER is set, when
@@ -49,33 +49,40 @@ struct PBASettings
  * Returns a pointer to PBASettings on success or NULL on failure.
  *
  * */
-struct PBASettings *pba_init(const char *pepper, unsigned int pepper_size, unsigned int count, char *prefix);
+struct PBASettings *
+pba_init (const char *pepper, unsigned int pepper_size, unsigned int count,
+          char *prefix);
 
 /* return values for pba pba_verify_hash */
-enum pba_rc {
-    VALID, /* hash and password are correct */
-    UPDATE_RECOMMENDED, /* password is correct but in an outdated format*/
-    INVALID, /* password is incorrect */
-    ERR, /* unexpected error */
+enum pba_rc
+{
+  VALID,              /* hash and password are correct */
+  UPDATE_RECOMMENDED, /* password is correct but in an outdated format*/
+  INVALID,            /* password is incorrect */
+  ERR,                /* unexpected error */
 };
 
 /**
  * pba_hash tries to create a hash based SETTING and PASSWORD.
  * Returns a hash on success or a NULL pointer on failure
  */
-char *pba_hash (struct PBASettings *setting, const char *password);
+char *
+pba_hash (struct PBASettings *setting, const char *password);
 
 /**
  * pba_verify_hash tries to create hash based on PASSWORD and settings found via
  * HASH and compares that with HASH.
  *
- * Returns VALID if HASH and PASSWORD are correct; 
- * UPDATE_RECOMMENDED when the HASH and PASSWORD are correct but based on a deprecated algorithm;
- * IVALID if HASH does not match PASSWORD;
- * ERR if an unexpected error occurs.
+ * Returns VALID if HASH and PASSWORD are correct;
+ * UPDATE_RECOMMENDED when the HASH and PASSWORD are correct but based on a
+ * deprecated algorithm; IVALID if HASH does not match PASSWORD; ERR if an
+ * unexpected error occurs.
  */
-enum pba_rc pba_verify_hash(const struct PBASettings *settings, const char *hash, const char *password);
+enum pba_rc
+pba_verify_hash (const struct PBASettings *settings, const char *hash,
+                 const char *password);
 
-void pba_finalize(struct PBASettings *settings);
+void
+pba_finalize (struct PBASettings *settings);
 
 #endif
--- util/passwordbasedauthentication_tests.c
+++ util/passwordbasedauthentication_tests.c
@@ -17,8 +17,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "passwordbasedauthentication.c"
 #include "authutils.h"
+#include "passwordbasedauthentication.c"
 
 #include <cgreen/cgreen.h>
 #include <cgreen/mocks.h>
@@ -33,95 +33,97 @@ AfterEach (PBA)
 
 Ensure (PBA, returns_false_on_not_phc_compliant_setting)
 {
-    assert_false(pba_is_phc_compliant("$"));
-    assert_false(pba_is_phc_compliant("password"));
+  assert_false (pba_is_phc_compliant ("$"));
+  assert_false (pba_is_phc_compliant ("password"));
 }
 Ensure (PBA, returns_true_on_phc_compliant_setting)
 {
-    assert_true(pba_is_phc_compliant("$password"));
+  assert_true (pba_is_phc_compliant ("$password"));
 }
 Ensure (PBA, returns_NULL_on_unsupport_settings)
 {
-    struct PBASettings setting = { "0000", 20000, "$6$"};
-    assert_false(pba_hash(NULL, "*password"));
-    assert_false(pba_hash(&setting, NULL));
-    setting.prefix = "$1$";
-    assert_false(pba_hash(&setting, "*password"));
+  struct PBASettings setting = {"0000", 20000, "$6$"};
+  assert_false (pba_hash (NULL, "*password"));
+  assert_false (pba_hash (&setting, NULL));
+  setting.prefix = "$1$";
+  assert_false (pba_hash (&setting, "*password"));
 }
 Ensure (PBA, unique_hash_without_adding_used_pepper)
 {
-    struct PBASettings setting = { "4242", 20000, "$6$"};
-    char *cmp_hash, *hash;
-    hash = pba_hash(&setting, "*password");
-    assert_not_equal(hash, NULL);
-    assert_false(string_contains(hash, setting.pepper));
-    cmp_hash = pba_hash(&setting, "*password");
-    assert_string_not_equal(hash, cmp_hash);
-    free(hash);
-    free(cmp_hash);
+  struct PBASettings setting = {"4242", 20000, "$6$"};
+  char *cmp_hash, *hash;
+  hash = pba_hash (&setting, "*password");
+  assert_not_equal (hash, NULL);
+  assert_false (string_contains (hash, setting.pepper));
+  cmp_hash = pba_hash (&setting, "*password");
+  assert_string_not_equal (hash, cmp_hash);
+  free (hash);
+  free (cmp_hash);
 }
 Ensure (PBA, verify_hash)
 {
-    struct PBASettings setting = { "4242" , 20000, "$6$"};
-    char *hash;
-    hash = pba_hash(&setting, "*password");
-    assert_not_equal(hash, NULL);
-    assert_equal(pba_verify_hash(&setting, hash, "*password"), VALID);
-    assert_equal(pba_verify_hash(&setting, hash, "*password1"), INVALID);
-    free(hash);
-    struct PBASettings setting_wo_pepper = { "\0\0\0\0" , 20000, "$6$"};
-    hash = pba_hash(&setting_wo_pepper, "*password");
-    assert_equal(pba_verify_hash(&setting_wo_pepper, hash, "*password"), VALID);
-    free(hash);
+  struct PBASettings setting = {"4242", 20000, "$6$"};
+  char *hash;
+  hash = pba_hash (&setting, "*password");
+  assert_not_equal (hash, NULL);
+  assert_equal (pba_verify_hash (&setting, hash, "*password"), VALID);
+  assert_equal (pba_verify_hash (&setting, hash, "*password1"), INVALID);
+  free (hash);
+  struct PBASettings setting_wo_pepper = {"\0\0\0\0", 20000, "$6$"};
+  hash = pba_hash (&setting_wo_pepper, "*password");
+  assert_equal (pba_verify_hash (&setting_wo_pepper, hash, "*password"), VALID);
+  free (hash);
 }
 
 Ensure (PBA, verify_hash_returns_invalid_on_np_hash_np_password)
 {
-    struct PBASettings setting = { "4242" , 20000, "$6$"};
-    char *hash;
-    hash = pba_hash(&setting, "*password");
-    assert_not_equal(hash, NULL);
-    assert_equal(pba_verify_hash(&setting, NULL, "*password"), INVALID);
-    assert_equal(pba_verify_hash(&setting, hash, NULL), INVALID);
+  struct PBASettings setting = {"4242", 20000, "$6$"};
+  char *hash;
+  hash = pba_hash (&setting, "*password");
+  assert_not_equal (hash, NULL);
+  assert_equal (pba_verify_hash (&setting, NULL, "*password"), INVALID);
+  assert_equal (pba_verify_hash (&setting, hash, NULL), INVALID);
 }
-
 
 Ensure (PBA, defaults)
 {
-    int i;
-    struct PBASettings *settings = pba_init(NULL, 0, 0, NULL);
-    assert_equal(settings->count, 20000);
-    for (i = 0; i < MAX_PEPPER_SIZE; i++)
-        assert_equal_with_message(settings->pepper[i], 0, "init_without_pepper_should_not_have_pepper");
-    assert_string_equal(settings->prefix, "$6$");
-    pba_finalize(settings);
-
+  int i;
+  struct PBASettings *settings = pba_init (NULL, 0, 0, NULL);
+  assert_equal (settings->count, 20000);
+  for (i = 0; i < MAX_PEPPER_SIZE; i++)
+    assert_equal_with_message (settings->pepper[i], 0,
+                               "init_without_pepper_should_not_have_pepper");
+  assert_string_equal (settings->prefix, "$6$");
+  pba_finalize (settings);
 }
 Ensure (PBA, initialization)
 {
-    int i;
-    struct PBASettings *settings = pba_init("444", 3, 1, "$6$");
-    assert_equal(settings->count, 1);
-    for (i = 0; i < MAX_PEPPER_SIZE - 1; i++)
-        assert_equal_with_message(settings->pepper[i], '4', "init_with_pepper_should_be_set");
-    assert_equal_with_message(settings->pepper[MAX_PEPPER_SIZE -1], '\0', "last_pepper_should_be_unset_by_pepper_3");
-    assert_string_equal(settings->prefix, "$6$");
-    pba_finalize(settings);
-    settings = pba_init("444", MAX_PEPPER_SIZE + 1, 1, "$6$");
-    assert_equal_with_message(settings, NULL, "should_fail_due_to_too_much_pepper");
-    settings = pba_init("444", MAX_PEPPER_SIZE, 1, "$WALDFEE$");
-    assert_equal_with_message(settings, NULL, "should_fail_due_to_unknown_prefix");
-
+  int i;
+  struct PBASettings *settings = pba_init ("444", 3, 1, "$6$");
+  assert_equal (settings->count, 1);
+  for (i = 0; i < MAX_PEPPER_SIZE - 1; i++)
+    assert_equal_with_message (settings->pepper[i], '4',
+                               "init_with_pepper_should_be_set");
+  assert_equal_with_message (settings->pepper[MAX_PEPPER_SIZE - 1], '\0',
+                             "last_pepper_should_be_unset_by_pepper_3");
+  assert_string_equal (settings->prefix, "$6$");
+  pba_finalize (settings);
+  settings = pba_init ("444", MAX_PEPPER_SIZE + 1, 1, "$6$");
+  assert_equal_with_message (settings, NULL,
+                             "should_fail_due_to_too_much_pepper");
+  settings = pba_init ("444", MAX_PEPPER_SIZE, 1, "$WALDFEE$");
+  assert_equal_with_message (settings, NULL,
+                             "should_fail_due_to_unknown_prefix");
 }
 
 Ensure (PBA, handle_md5_hash)
 {
-    struct PBASettings *settings = pba_init(NULL, 0, 0, NULL);
-    char *hash;
-    assert_equal(gvm_auth_init(), 0);
-    hash = get_password_hashes ("admin");
-    assert_equal(pba_verify_hash(settings, hash, "admin"), UPDATE_RECOMMENDED);
-    pba_finalize(settings);
+  struct PBASettings *settings = pba_init (NULL, 0, 0, NULL);
+  char *hash;
+  assert_equal (gvm_auth_init (), 0);
+  hash = get_password_hashes ("admin");
+  assert_equal (pba_verify_hash (settings, hash, "admin"), UPDATE_RECOMMENDED);
+  pba_finalize (settings);
 }
 
 int
@@ -133,22 +135,15 @@ main (int argc, char **argv)
 
   add_test_with_context (suite, PBA,
                          returns_false_on_not_phc_compliant_setting);
-  add_test_with_context (suite, PBA,
-                         returns_true_on_phc_compliant_setting);
-  add_test_with_context (suite, PBA,
-                         returns_NULL_on_unsupport_settings);
-  add_test_with_context (suite, PBA,
-                         unique_hash_without_adding_used_pepper);
-  add_test_with_context (suite, PBA,
-                         verify_hash);
+  add_test_with_context (suite, PBA, returns_true_on_phc_compliant_setting);
+  add_test_with_context (suite, PBA, returns_NULL_on_unsupport_settings);
+  add_test_with_context (suite, PBA, unique_hash_without_adding_used_pepper);
+  add_test_with_context (suite, PBA, verify_hash);
   add_test_with_context (suite, PBA,
                          verify_hash_returns_invalid_on_np_hash_np_password);
-  add_test_with_context (suite, PBA,
-                         handle_md5_hash);
-  add_test_with_context (suite, PBA,
-                         defaults);
-  add_test_with_context (suite, PBA,
-                         initialization);
+  add_test_with_context (suite, PBA, handle_md5_hash);
+  add_test_with_context (suite, PBA, defaults);
+  add_test_with_context (suite, PBA, initialization);
   if (argc > 1)
     return run_single_test (suite, argv[1], create_text_reporter ());
   return run_test_suite (suite, create_text_reporter ());
