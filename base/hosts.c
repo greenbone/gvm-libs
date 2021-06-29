f39348c69db9986a36909c328798b9ef17604b3e
ArnoStiefvater <arno.stiefvater@greenbone.net>
2021-06-29 07:06:30 +0200

    Fix formatting

--- base/hosts.c
+++ base/hosts.c
@@ -958,7 +958,7 @@ gvm_hosts_add (gvm_hosts_t *hosts, gvm_host_t *host)
       hosts->hosts =
         g_realloc_n (hosts->hosts, hosts->max_size, sizeof (*hosts->hosts));
       memset (hosts->hosts + hosts->count, '\0',
-              (hosts->max_size - hosts->count) * sizeof (gvm_host_t*));
+              (hosts->max_size - hosts->count) * sizeof (gvm_host_t *));
     }
   hosts->hosts[hosts->count] = host;
   hosts->count++;
