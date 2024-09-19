a041b8b9939b3da373f3d438fd6ceddc45c0826d
Johannes Helmold <johannes.helmold@greenbone.net>
2024-09-19 17:17:02 +0200

    Add: Added functions for the new CPE matching in CVE scans.

--- util/cpeutils.h
+++ util/cpeutils.h
@@ -38,7 +38,13 @@ char *
 uri_cpe_to_fs_cpe (const char *);
 
 char *
+uri_cpe_to_fs_product (const char *);
+
+char *
 fs_cpe_to_uri_cpe (const char *);
+
+char *
+fs_cpe_to_uri_product (const char *);
 
 void
 uri_cpe_to_cpe_struct (const char *, cpe_struct_t *);
@@ -46,44 +52,17 @@ uri_cpe_to_cpe_struct (const char *, cpe_struct_t *);
 char *
 cpe_struct_to_uri_cpe (const cpe_struct_t *);
 
+char *
+cpe_struct_to_uri_product (const cpe_struct_t *);
+
 void
 fs_cpe_to_cpe_struct (const char *, cpe_struct_t *);
 
 char *
 cpe_struct_to_fs_cpe (const cpe_struct_t *);
 
-static char *
-get_uri_component (const char *, int);
-
-static char *
-decode_uri_component (const char *);
-
-static void
-unpack_sixth_uri_component (const char *, cpe_struct_t *);
-
-static char *
-get_fs_component (const char *, int);
-
-static char *
-unbind_fs_component (char *);
-
-static char *
-add_quoting (const char *);
-
-static char *
-bind_cpe_component_for_uri (const char *);
-
-static char *
-transform_for_uri (const char *);
-
-static char *
-pack_sixth_uri_component (const cpe_struct_t *);
-
-static char *
-bind_cpe_component_for_fs (const char *);
-
-static char *
-process_quoted_chars (const char *);
+char *
+cpe_struct_to_fs_product (const cpe_struct_t *);
 
 void
 cpe_struct_init (cpe_struct_t *);
@@ -91,14 +70,8 @@ cpe_struct_init (cpe_struct_t *);
 void
 cpe_struct_free (cpe_struct_t *);
 
-static void
-trim_pct (char *);
-
-static void
-get_code (char *, const char *);
-
-static void
-str_cpy (char **, const char *, int);
+gboolean
+cpe_struct_match (cpe_struct_t source, cpe_struct_t target);
 
 enum set_relation
 {
@@ -109,27 +82,4 @@ enum set_relation
   UNDEFINED
 };
 
-gboolean
-cpe_struct_match (cpe_struct_t source, cpe_struct_t target);
-
-static enum set_relation
-compare_component (const char *, const char *);
-
-static enum set_relation
-compare_strings (const char *, const char *);
-
-static int
-count_escapes (const char *, int, int);
-
-static gboolean
-is_even_wildcards (const char *, int);
-
-static gboolean
-has_wildcards (const char *);
-
-static int
-index_of (const char *, const char *, int);
-
-static gboolean
-is_string (const char *);
 #endif
