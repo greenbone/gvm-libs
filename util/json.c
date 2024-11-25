8bc8444b725ae96b3f83e3c6c62ca4fc4b2cc6b3
Matt Mundell <matt@mundell.me>
2024-11-25 17:13:04 +0200

    Add: group common json utilities

---
+++ util/json.c
@@ -0,0 +1,6 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "json.h"
---
+++ util/json.h
@@ -0,0 +1,13 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef _GVM_JSON_H
+#define _GVM_JSON_H
+
+#define _GNU_SOURCE
+
+#include <cjson/cJSON.h>
+
+#endif /* _GVM_JSON_H */
