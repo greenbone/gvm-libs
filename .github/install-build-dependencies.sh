44aa9d7cd0db1d1ec3dcab527b07e0dac152eb7f
Björn Ricks <bjoern.ricks@greenbone.net>
2025-01-20 09:42:33 +0100

    Install the same libcurl version in all builds
    We are going to use libcurl with GnuTLS.

--- .github/install-build-dependencies.sh
+++ .github/install-build-dependencies.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 # This script installs gvm-libs dependencies
 set -ex
+
 apt-get update && \
-  apt-get install -y --no-install-recommends \
+apt-get install -y --no-install-recommends  --no-install-suggests \
   build-essential \
   cmake \
   curl \
   gnupg \
   lcov \
   libcjson-dev \
-  libcurl4-openssl-dev \
+  libcurl4-gnutls-dev \
   libgcrypt-dev \
   libglib2.0-dev \
   libgnutls28-dev \
