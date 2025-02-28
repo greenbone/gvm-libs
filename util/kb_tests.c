50b4c1d021474aad66153b05056a75ead3d5c159
Matt Mundell <matt@mundell.me>
2025-02-28 18:00:29 +0200

    Add: first tests of kb.c

---
+++ util/kb_tests.c
@@ -0,0 +1,70 @@
+/* SPDX-FileCopyrightText: 2019-2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "kb.c"
+
+#include <cgreen/assertions.h>
+#include <cgreen/cgreen.h>
+#include <cgreen/constraint_syntax_helpers.h>
+#include <cgreen/internal/c_assertions.h>
+#include <cgreen/mocks.h>
+
+#define TCP "tcp://"
+
+Describe (kb);
+BeforeEach (kb)
+{
+}
+
+AfterEach (kb)
+{
+}
+
+/* parse_port_of_addr */
+
+Ensure (kb, parse_port_of_addr)
+{
+  char *addr, *port;
+
+  addr = TCP "xxx:5";
+  port = parse_port_of_addr (addr, strlen (TCP));
+  assert_that (port, is_equal_to_string ("5"));
+}
+
+Ensure (kb, parse_port_of_addr_missing)
+{
+  char *addr, *port;
+
+  addr = TCP "xxx";
+  port = parse_port_of_addr (addr, strlen (TCP));
+  assert_that (port, is_null);
+}
+
+Ensure (kb, parse_port_of_addr_v6)
+{
+  char *addr, *port;
+
+  addr = TCP "[2001:db8::1]:8080";
+  port = parse_port_of_addr (addr, strlen (TCP));
+  assert_that (port, is_equal_to_string ("8080"));
+}
+
+/* Test suite. */
+int
+main (int argc, char **argv)
+{
+  TestSuite *suite;
+
+  suite = create_test_suite ();
+
+  add_test_with_context (suite, kb, parse_port_of_addr);
+  add_test_with_context (suite, kb, parse_port_of_addr_missing);
+  add_test_with_context (suite, kb, parse_port_of_addr_v6);
+
+  if (argc > 1)
+    return run_single_test (suite, argv[1], create_text_reporter ());
+
+  return run_test_suite (suite, create_text_reporter ());
+}
