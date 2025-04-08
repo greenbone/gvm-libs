3a1f57290b1f8568bc79f27334f1bc7ef16cd008
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2025-04-08 17:40:31 +0200

    Change: Move VT JSON parsing to a separate util library. Added return values on success/error/end when parsing vts.

---
+++ util/vtparser.h
@@ -0,0 +1,47 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/**
+ * @file
+ * @brief Simple JSON reader.
+ */
+
+#ifndef _GVM_VTPARSER_H
+#define _GVM_VTPARSER_H
+
+#define _GNU_SOURCE /* See feature_test_macros(7) */
+#define _FILE_OFFSET_BITS 64
+#include "../base/cvss.h"
+#include "../base/nvti.h" /* for nvti_t */
+#include "../util/jsonpull.h"
+
+#include <cjson/cJSON.h>
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * @brief VT categories
+ */
+typedef enum
+{
+  ACT_INIT = 0,
+  ACT_SCANNER,
+  ACT_SETTINGS,
+  ACT_GATHER_INFO,
+  ACT_ATTACK,
+  ACT_MIXED_ATTACK,
+  ACT_DESTRUCTIVE_ATTACK,
+  ACT_DENIAL,
+  ACT_KILL_HOST,
+  ACT_FLOOD,
+  ACT_END,
+} nvt_category;
+
+int
+parse_vt_json (gvm_json_pull_parser_t *, gvm_json_pull_event_t *, nvti_t **);
+
+#endif /* _GVM_VTPARSER_H */
