93e86602b0a850e644b9f7b8079c727d60f3c6a2
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-10 14:25:31 +0200

    misc: Use lower case name for cmake add_unit_test macro
    The gersemi formatter uses the macro name in a CMakeLists.txt files.

--- cmake/MacroAddUnitTest.cmake
+++ cmake/MacroAddUnitTest.cmake
@@ -1,6 +1,6 @@
-macro (ADD_UNIT_TEST _testName _testSource)
+macro (add_unit_test _testName _testSource)
   add_executable(${_testName} ${_testSource})
   target_link_libraries(${_testName} ${CGREEN_LIBRARIES} ${ARGN})
   target_include_directories(${_testName} PRIVATE ${CGREEN_INCLUDE_DIRS})
   add_test(NAME ${_testName} COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${_testName})
-endmacro (ADD_UNIT_TEST)
+endmacro ()
