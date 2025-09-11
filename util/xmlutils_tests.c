6b3e960b27ae9f982f28704e075bd01431f6032b
Matt Mundell <matt@mundell.me>
2025-09-11 12:22:09 +0200

    Fix: free element_text in XML tests

--- util/xmlutils_tests.c
+++ util/xmlutils_tests.c
@@ -199,6 +199,7 @@ Ensure (xmlutils, parse_element_parses_simple_xml)
 {
   element_t element, b;
   const gchar *xml;
+  gchar *text;
 
   xml = "<a><b>1</b></a>";
 
@@ -209,7 +210,9 @@ Ensure (xmlutils, parse_element_parses_simple_xml)
   b = element_child (element, "b");
   assert_that (element_name (b), is_equal_to_string ("b"));
 
-  assert_that (element_text (b), is_equal_to_string ("1"));
+  text = element_text (b);
+  assert_that (text, is_equal_to_string ("1"));
+  g_free (text);
 
   element_free (element);
 }
@@ -234,6 +237,7 @@ Ensure (xmlutils, parse_element_handles_declaration)
 {
   element_t element, b;
   const gchar *xml;
+  gchar *text;
 
   xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><a><b ba1='test'>1</b></a>";
 
@@ -244,7 +248,9 @@ Ensure (xmlutils, parse_element_handles_declaration)
   b = element_child (element, "b");
   assert_that (element_name (b), is_equal_to_string ("b"));
 
-  assert_that (element_text (b), is_equal_to_string ("1"));
+  text = element_text (b);
+  assert_that (text, is_equal_to_string ("1"));
+  g_free (text);
 
   element_free (element);
 }
@@ -253,6 +259,7 @@ Ensure (xmlutils, parse_element_handles_namespace)
 {
   element_t element, b;
   const gchar *xml;
+  gchar *text;
 
   xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><a><n:b ba1='test' "
         "n2:ba2='test2'>1</n:b></a>";
@@ -264,7 +271,9 @@ Ensure (xmlutils, parse_element_handles_namespace)
   b = element_child (element, "n:b");
   assert_that (element_name (b), is_equal_to_string ("n:b"));
 
-  assert_that (element_text (b), is_equal_to_string ("1"));
+  text = element_text (b);
+  assert_that (text, is_equal_to_string ("1"));
+  g_free (text);
 
   assert_that (element_attribute (b, "n2:ba2"), is_equal_to_string ("test2"));
 
@@ -273,7 +282,7 @@ Ensure (xmlutils, parse_element_handles_namespace)
 
 Ensure (xmlutils, parse_element_oval_timestamp)
 {
-  gchar *generator_name;
+  gchar *generator_name, *text;
   element_t generator, timestamp, element;
   const gchar *xml;
 
@@ -310,8 +319,10 @@ Ensure (xmlutils, parse_element_oval_timestamp)
   assert_that (generator, is_not_null);
   timestamp = element_child (generator, "oval:timestamp");
   assert_that (timestamp, is_not_null);
-  assert_that (element_text (timestamp),
-               is_equal_to_string ("2015-08-20T10:09:07.183-04:00"));
+
+  text = element_text (timestamp);
+  assert_that (text, is_equal_to_string ("2015-08-20T10:09:07.183-04:00"));
+  g_free (text);
 
   element_free (element);
 }
@@ -413,6 +424,7 @@ Ensure (xmlutils, parse_element_item_handles_cdata)
 {
   element_t element;
   const gchar *xml;
+  gchar *text;
 
   xml =
     "<description><![CDATA[Several vulnerabilities were discovered in the "
@@ -449,8 +461,9 @@ Ensure (xmlutils, parse_element_item_handles_cdata)
 
   assert_that (parse_element (xml, &element), is_equal_to (0));
   assert_that (element_name (element), is_equal_to_string ("description"));
+  text = element_text (element);
   assert_that (
-    element_text (element),
+    text,
     is_equal_to_string (
       "Several vulnerabilities were discovered in the Chromium browser. The "
       "Common Vulnerabilities and Exposures project identifies the following "
@@ -483,6 +496,7 @@ Ensure (xmlutils, parse_element_item_handles_cdata)
       "[70027] Cross-origin error message leak with workers. [70336] Stale "
       "pointer in table painting. [72028] Stale pointer with SVG cursors. "
       "[73746]"));
+  g_free (text);
   element_free (element);
 }
 
@@ -492,6 +506,7 @@ Ensure (xmlutils, element_next_handles_multiple_children)
 {
   element_t element, child;
   const gchar *xml;
+  gchar *text;
 
   xml = "<top><a>1</a><b>2</b><c>3</c></top>";
 
@@ -502,17 +517,23 @@ Ensure (xmlutils, element_next_handles_multiple_children)
   child = element_first_child (element);
   assert_that (child, is_not_null);
   assert_that (element_name (child), is_equal_to_string ("a"));
-  assert_that (element_text (child), is_equal_to_string ("1"));
+  text = element_text (child);
+  assert_that (text, is_equal_to_string ("1"));
+  g_free (text);
 
   child = element_next (child);
   assert_that (child, is_not_null);
   assert_that (element_name (child), is_equal_to_string ("b"));
-  assert_that (element_text (child), is_equal_to_string ("2"));
+  text = element_text (child);
+  assert_that (text, is_equal_to_string ("2"));
+  g_free (text);
 
   child = element_next (child);
   assert_that (child, is_not_null);
   assert_that (element_name (child), is_equal_to_string ("c"));
-  assert_that (element_text (child), is_equal_to_string ("3"));
+  text = element_text (child);
+  assert_that (text, is_equal_to_string ("3"));
+  g_free (text);
 
   element_free (element);
 }
@@ -554,6 +575,7 @@ Ensure (xmlutils, print_element_to_string_prints)
 
 Ensure (xmlutils, depth1_returns_top_level_children_in_order)
 {
+  gchar *text;
   const char *xml = "<root>"
                     "  <a x='1'>A</a>"
                     "  <b>B</b>"
@@ -574,14 +596,18 @@ Ensure (xmlutils, depth1_returns_top_level_children_in_order)
   assert_that (err, is_null);
   assert_that (e, is_not_null);
   assert_that (element_name (e), is_equal_to_string ("a"));
-  assert_that (element_text (e), is_equal_to_string ("A"));
+  text = element_text (e);
+  assert_that (text, is_equal_to_string ("A"));
+  g_free (text);
   element_free (e);
 
   e = xml_file_iterator_next (it, &err);
   assert_that (err, is_null);
   assert_that (e, is_not_null);
   assert_that (element_name (e), is_equal_to_string ("b"));
-  assert_that (element_text (e), is_equal_to_string ("B"));
+  text = element_text (e);
+  assert_that (text, is_equal_to_string ("B"));
+  g_free (text);
   element_free (e);
 
   e = xml_file_iterator_next (it, &err);
@@ -590,7 +616,9 @@ Ensure (xmlutils, depth1_returns_top_level_children_in_order)
   assert_that (element_name (e), is_equal_to_string ("c"));
   element_t d = element_child (e, "d");
   assert_that (d, is_not_null);
-  assert_that (element_text (d), is_equal_to_string ("D"));
+  text = element_text (d);
+  assert_that (text, is_equal_to_string ("D"));
+  g_free (text);
   element_free (e);
 
   e = xml_file_iterator_next (it, &err);
