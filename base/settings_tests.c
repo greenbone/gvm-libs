5cad5208a0e52f5bdf3930db499dce79f903d93f
Matt Mundell <matt@mundell.me>
2025-10-03 10:30:41 +0200

    Also tests setting groups

--- base/settings_tests.c
+++ base/settings_tests.c
@@ -117,6 +117,69 @@ Ensure (settings, settings_iterator_operations)
   g_remove (config_file);
 }
 
+Ensure (settings, settings_group_handling)
+{
+  settings_iterator_t iterator;
+  int ret;
+  gchar *config_file = "test_group_settings.conf";
+  FILE *file;
+  const gchar *name;
+  const gchar *value;
+
+  // Create a temporary configuration file with multiple groups
+  file = fopen (config_file, "w");
+  assert_that (file, is_not_null);
+  fprintf (file, "[group1]\nkey1=value1\nkey2=value2\n\n[group2]\nkey3=value3\nkey4=value4\n");
+  fclose (file);
+
+  // Initialize iterator from file for group1
+  ret = init_settings_iterator_from_file (&iterator, config_file, "group1");
+  assert_that (ret, is_equal_to (0));
+
+  // Test iteration for group1
+  assert_that (settings_iterator_next (&iterator), is_true);
+  name = settings_iterator_name (&iterator);
+  value = settings_iterator_value (&iterator);
+  assert_that (name, is_equal_to_string ("key1"));
+  assert_that (value, is_equal_to_string ("value1"));
+
+  assert_that (settings_iterator_next (&iterator), is_true);
+  name = settings_iterator_name (&iterator);
+  value = settings_iterator_value (&iterator);
+  assert_that (name, is_equal_to_string ("key2"));
+  assert_that (value, is_equal_to_string ("value2"));
+
+  // No more items in group1
+  assert_that (settings_iterator_next (&iterator), is_false);
+
+  // Clean up iterator
+  cleanup_settings_iterator (&iterator);
+
+  // Initialize iterator from file for group2
+  ret = init_settings_iterator_from_file (&iterator, config_file, "group2");
+  assert_that (ret, is_equal_to (0));
+
+  // Test iteration for group2
+  assert_that (settings_iterator_next (&iterator), is_true);
+  name = settings_iterator_name (&iterator);
+  value = settings_iterator_value (&iterator);
+  assert_that (name, is_equal_to_string ("key3"));
+  assert_that (value, is_equal_to_string ("value3"));
+
+  assert_that (settings_iterator_next (&iterator), is_true);
+  name = settings_iterator_name (&iterator);
+  value = settings_iterator_value (&iterator);
+  assert_that (name, is_equal_to_string ("key4"));
+  assert_that (value, is_equal_to_string ("value4"));
+
+  // No more items in group2
+  assert_that (settings_iterator_next (&iterator), is_false);
+
+  // Clean up
+  cleanup_settings_iterator (&iterator);
+  g_remove (config_file);
+}
+
 Ensure (settings, settings_cleanup_function)
 {
   settings_t settings;
@@ -140,6 +203,7 @@ main (int argc, char **argv)
   add_test_with_context (suite, settings, init_settings_iterator_from_nonexistent_file);
   add_test_with_context (suite, settings, init_settings_iterator_from_valid_file);
   add_test_with_context (suite, settings, settings_iterator_operations);
+  add_test_with_context (suite, settings, settings_group_handling);
   add_test_with_context (suite, settings, settings_cleanup_function);
 
   if (argc > 1)
