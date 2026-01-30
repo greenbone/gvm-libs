7b8aed177d6390fba2f30cbccd832264c5e18ebe
Johannes Helmold <johannes.helmold@greenbone.net>
2026-01-30 08:43:52 +0100

    Kept hosts_ordering for reasons of compatibility.

--- gmp/gmp.h
+++ gmp/gmp.h
@@ -159,17 +159,18 @@ static const gmp_get_task_opts_t gmp_get_task_opts_defaults = {NULL, NULL, 0,
  */
 typedef struct
 {
-  array_t *alert_ids;       ///< Array of alert IDs.
-  const char *config_id;    ///< ID of config.
-  const char *scanner_id;   ///< ID of task scanner.
-  const char *schedule_id;  ///< ID of task schedule.
-  const char *slave_id;     ///< ID of task schedule.
-  const char *target_id;    ///< ID of target.
-  const char *name;         ///< Name of task.
-  const char *comment;      ///< Comment on task.
-  const char *observers;    ///< Comma-separated string of observer users.
-  array_t *observer_groups; ///< IDs of observer groups.
-  int schedule_periods;     ///< Number of periods the schedule must run for.
+  array_t *alert_ids;         ///< Array of alert IDs.
+  const char *config_id;      ///< ID of config.
+  const char *scanner_id;     ///< ID of task scanner.
+  const char *schedule_id;    ///< ID of task schedule.
+  const char *slave_id;       ///< ID of task schedule.
+  const char *target_id;      ///< ID of target.
+  const char *name;           ///< Name of task.
+  const char *comment;        ///< Comment on task.
+  const char *hosts_ordering; ///< Not used, kept for reasons of compatibility.
+  const char *observers;      ///< Comma-separated string of observer users.
+  array_t *observer_groups;   ///< IDs of observer groups.
+  int schedule_periods;       ///< Number of periods the schedule must run for.
   /* Preferences */
   const char *in_assets;    ///< In assets preference.
   const char *max_hosts;    ///< Max hosts preference.
@@ -183,7 +184,7 @@ typedef struct
  * @brief Sensible default values for gmp_get_report_opts_t.
  */
 static const gmp_create_task_opts_t gmp_create_task_opts_defaults = {
-  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, 0,    NULL, NULL, NULL, NULL, 0};
 
 /**
