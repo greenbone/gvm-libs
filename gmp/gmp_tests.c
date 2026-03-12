0db48004ccb44552d44486a301787bf3705b5311
ozgen <ozgn45@gmail.com>
2026-03-12 15:23:55 +0100

    test:  add unit tests for extended authenticate info flow

--- gmp/gmp_tests.c
+++ gmp/gmp_tests.c
@@ -139,6 +139,13 @@ add_mock_attribute (entity_t entity, const char *name, const char *value)
   g_hash_table_insert (entity->attributes, g_strdup (name), g_strdup (value));
 }
 
+/* Helper function to add parent to mock entity */
+static void
+add_mock_child (entity_t parent, entity_t child)
+{
+  parent->entities = g_slist_append (parent->entities, child);
+}
+
 /* gmp_task_status */
 
 Ensure (gmp, gmp_task_status_returns_correct_string_for_valid_response)
@@ -354,6 +361,210 @@ Ensure (gmp, gmp_ping_returns_error_with_invalid_status)
   assert_that (result, is_equal_to (-1));
 }
 
+Ensure (gmp, gmp_authenticate_info_ext_c_returns_success_and_sets_outputs)
+{
+  entity_t response = create_mock_entity ("authenticate_response", "");
+  add_mock_attribute (response, "status", "200");
+
+  entity_t timezone = create_mock_entity ("timezone", "Europe/Berlin");
+  entity_t role = create_mock_entity ("role", "Admin");
+  entity_t pw_warning =
+    create_mock_entity ("password_warning", "Password is short");
+  entity_t token = create_mock_entity ("token", "jwt-token-value");
+
+  add_mock_child (response, timezone);
+  add_mock_child (response, role);
+  add_mock_child (response, pw_warning);
+  add_mock_child (response, token);
+
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (response));
+
+  char *out_role = NULL;
+  char *out_timezone = NULL;
+  char *out_pw_warning = NULL;
+  char *out_jwt = NULL;
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+  opts.role = &out_role;
+  opts.timezone = &out_timezone;
+  opts.pw_warning = &out_pw_warning;
+  opts.jwt_requested = 1;
+  opts.jwt = &out_jwt;
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (0));
+  assert_that (out_role, is_equal_to_string ("Admin"));
+  assert_that (out_timezone, is_equal_to_string ("Europe/Berlin"));
+  assert_that (out_pw_warning, is_equal_to_string ("Password is short"));
+  assert_that (out_jwt, is_equal_to_string ("jwt-token-value"));
+
+  g_free (out_role);
+  g_free (out_timezone);
+  g_free (out_pw_warning);
+  g_free (out_jwt);
+}
+
+Ensure (gmp, gmp_authenticate_info_ext_c_succeeds_without_optional_outputs)
+{
+  entity_t response = create_mock_entity ("authenticate_response", "");
+  add_mock_attribute (response, "status", "200");
+
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (response));
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (0));
+}
+
+Ensure (gmp, gmp_authenticate_info_ext_c_returns_send_error)
+{
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (-1));
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.username = "admin";
+  opts.password = "secret";
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (-1));
+}
+
+Ensure (gmp, gmp_authenticate_info_ext_c_returns_timeout_on_try_read_timeout)
+{
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (-4));
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (3));
+}
+
+Ensure (gmp, gmp_authenticate_info_ext_c_returns_error_on_read_failure)
+{
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (-1));
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (-1));
+}
+
+Ensure (gmp, gmp_authenticate_info_ext_c_returns_error_when_status_empty)
+{
+  entity_t response = create_mock_entity ("authenticate_response", "");
+  add_mock_attribute (response, "status", "");
+
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (response));
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (-1));
+}
+
+Ensure (gmp, gmp_authenticate_info_ext_c_returns_2_on_auth_failure)
+{
+  entity_t response = create_mock_entity ("authenticate_response", "");
+  add_mock_attribute (response, "status", "400");
+
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (response));
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "wrong";
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (2));
+}
+
+Ensure (gmp,
+        gmp_authenticate_info_ext_c_leaves_jwt_null_when_requested_but_missing)
+{
+  entity_t response = create_mock_entity ("authenticate_response", "");
+  add_mock_attribute (response, "status", "200");
+
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (response));
+
+  char *out_jwt = (char *) 0x1;
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+  opts.jwt_requested = 1;
+  opts.jwt = &out_jwt;
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (0));
+  assert_that (out_jwt, is_null);
+}
+
+Ensure (gmp,
+        gmp_authenticate_info_ext_c_leaves_optional_outputs_null_when_absent)
+{
+  entity_t response = create_mock_entity ("authenticate_response", "");
+  add_mock_attribute (response, "status", "200");
+
+  expect (__wrap_gvm_connection_sendf_xml_quiet, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (0));
+  expect (__wrap_try_read_entity_c, will_return (response));
+
+  char *out_role = (char *) 0x1;
+  char *out_timezone = (char *) 0x1;
+  char *out_pw_warning = (char *) 0x1;
+
+  gmp_authenticate_info_opts_t opts = gmp_authenticate_info_opts_defaults;
+  opts.timeout = 10;
+  opts.username = "admin";
+  opts.password = "secret";
+  opts.role = &out_role;
+  opts.timezone = &out_timezone;
+  opts.pw_warning = &out_pw_warning;
+
+  int result = gmp_authenticate_info_ext_c (NULL, opts);
+
+  assert_that (result, is_equal_to (0));
+  assert_that (out_role, is_null);
+  assert_that (out_timezone, is_null);
+  assert_that (out_pw_warning, is_null);
+}
+
 /* Test suite. */
 
 int
@@ -392,6 +603,27 @@ main (int argc, char **argv)
   add_test_with_context (suite, gmp, gmp_ping_returns_error_on_read_failure);
   add_test_with_context (suite, gmp,
                          gmp_ping_returns_error_with_invalid_status);
+  add_test_with_context (
+    suite, gmp, gmp_authenticate_info_ext_c_returns_success_and_sets_outputs);
+  add_test_with_context (
+    suite, gmp, gmp_authenticate_info_ext_c_succeeds_without_optional_outputs);
+  add_test_with_context (suite, gmp,
+                         gmp_authenticate_info_ext_c_returns_send_error);
+  add_test_with_context (
+    suite, gmp,
+    gmp_authenticate_info_ext_c_returns_timeout_on_try_read_timeout);
+  add_test_with_context (
+    suite, gmp, gmp_authenticate_info_ext_c_returns_error_on_read_failure);
+  add_test_with_context (
+    suite, gmp, gmp_authenticate_info_ext_c_returns_error_when_status_empty);
+  add_test_with_context (suite, gmp,
+                         gmp_authenticate_info_ext_c_returns_2_on_auth_failure);
+  add_test_with_context (
+    suite, gmp,
+    gmp_authenticate_info_ext_c_leaves_jwt_null_when_requested_but_missing);
+  add_test_with_context (
+    suite, gmp,
+    gmp_authenticate_info_ext_c_leaves_optional_outputs_null_when_absent);
 
   if (argc > 1)
     ret = run_single_test (suite, argv[1], create_text_reporter ());
