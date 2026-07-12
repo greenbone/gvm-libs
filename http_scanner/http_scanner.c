4d3369ca9ef1c8dc3ec51e57d0b81684bd9c815c
Matt Mundell <matt@mundell.me>
2026-07-12 11:49:02 +0200

    Fix: free response body in http_scanner_stop_scan

--- http_scanner/http_scanner.c
+++ http_scanner/http_scanner.c
@@ -800,6 +800,7 @@ http_scanner_stop_scan (http_scanner_connector_t conn)
 
   g_string_free (path, TRUE);
 
+  g_free (response->body);
   if (response->code != RESP_CODE_ERR)
     response->body = g_strdup (http_scanner_stream_str (conn));
 
