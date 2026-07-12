1de6774e0bca40a4554e7d9058947166f8964f0f
Matt Mundell <matt@mundell.me>
2026-07-12 12:08:24 +0200

    Fix: free response body in http_scanner_get_health_started

--- http_scanner/http_scanner.c
+++ http_scanner/http_scanner.c
@@ -1343,6 +1343,7 @@ http_scanner_get_health_started (http_scanner_connector_t conn)
 
   g_string_free (path, TRUE);
 
+  g_free (response->body);
   if (response->code != RESP_CODE_ERR)
     response->body = g_strdup (http_scanner_stream_str (conn));
   else
