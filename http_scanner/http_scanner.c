e3b724a66f481842b51dba9f2e6333e76aedff54
Matt Mundell <matt@mundell.me>
2026-07-12 10:41:40 +0200

    Fix: free response body in http_scanner_delete_scan

--- http_scanner/http_scanner.c
+++ http_scanner/http_scanner.c
@@ -1217,6 +1217,7 @@ http_scanner_delete_scan (http_scanner_connector_t conn)
                                         NULL, NULL);
   g_string_free (path, TRUE);
 
+  g_free (response->body);
   if (response->code != RESP_CODE_ERR)
     response->body = g_strdup (http_scanner_stream_str (conn));
   else
